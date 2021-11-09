1ab4cd948082b2872e90b1c8922a0b4bbaf6316c
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-09 23:41:54 +0100

    Add name

--- App/uke_mlab/lib/main.dart
+++ App/uke_mlab/lib/main.dart
@@ -77,6 +77,9 @@ class MyApp extends StatelessWidget {
                             const Text('Fynn',
                                 style: TextStyle(fontSize: 24),
                                 textAlign: TextAlign.center),
+                            const Text('Mudassar',
+                                style: TextStyle(fontSize: 24),
+                                textAlign: TextAlign.center),
                           ])),
                       Positioned(
                           top: -100,
