0da698ab112a3d0a65ccb052aee33fe480ae9fe4
Anni <anni@rb-reinert.de>
2021-11-18 19:15:33 +0100

    [TASK] #29 Added 2 icons, aligned button content

--- App/uke_mlab/lib/src/views/startScreen.dart
+++ App/uke_mlab/lib/src/views/startScreen.dart
@@ -2,19 +2,24 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_grid/responsive_grid.dart';
+
 
 class startScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveGridRow(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         ResponsiveGridCol(
             md: 6,
-            child: Column(children: const [
-              startScreenButton(name: 'Adultus'),
-              startScreenButton(name: 'Not Adult'),
-              startScreenButton(name: 'Neither Adult'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+              startScreenButton(name: 'Adult', image: 'lib/src/assets/icons/children.svg'),
+              startScreenButton(name: 'Children', image: 'lib/src/assets/icons/children.svg'),
+              startScreenButton(name: 'Infant', image: 'lib/src/assets/icons/newborn.svg'),
             ])),
         ResponsiveGridCol(
           md: 6,
@@ -29,20 +34,40 @@ class startScreen extends StatelessWidget {
 
 class startScreenButton extends StatelessWidget {
   final String name;
+  final String image;
   const startScreenButton({
     required this.name,
+    required this.image,
   });
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(75), color: Color(0xffeeeeee)),
-      margin: const EdgeInsets.only(bottom: 12.0),
-      width: 800,
-      height: 155,
-      child: ElevatedButton(
-        onPressed: null,
-        child: Text(name),
+      return Container(
+        margin: EdgeInsets.only(bottom: 12.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size (800/MediaQuery.of(context).devicePixelRatio, 155/MediaQuery.of(context).devicePixelRatio),
+            primary: Color(0xffeeeeee),
+            onPrimary: Colors.black,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(75)),
+          ),
+          onPressed: (){ print (name);},
+          child: Row(
+            children: [
+              Expanded(
+                flex: 4,
+                child: SvgPicture.asset(image, height: 65,)),
+              Expanded(
+                flex: 5,
+                child: Text(
+                  name, 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25))),
+              Expanded(
+                flex: 4,
+                child: Container(), //Fynn bitte nicht töten, wir wissen nicht wie sonst :(
+                ),
+            ],
+          ),
       ),
     );
   }
