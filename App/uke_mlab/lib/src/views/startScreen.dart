5d8a813adf18008381901ef0d6e89689fac7921e
Anni <anni@rb-reinert.de>
2021-11-18 19:15:33 +0100

    [TASK] #29 Added 2 icons, aligned button content

--- App/uke_mlab/lib/src/views/startScreen.dart
+++ App/uke_mlab/lib/src/views/startScreen.dart
@@ -3,18 +3,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 237c277 ([TASK] #29 Added 2 icons, aligned button content)
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_grid/responsive_grid.dart';
 
 
+<<<<<<< HEAD
 =======
 import 'package:responsive_grid/responsive_grid.dart';
 
 >>>>>>> a555e76 ([Task] #29 Adding Start Screen first draft)
+=======
+>>>>>>> 237c277 ([TASK] #29 Added 2 icons, aligned button content)
 class startScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ResponsiveGridRow(
+<<<<<<< HEAD
 <<<<<<< HEAD
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -35,6 +42,18 @@ class startScreen extends StatelessWidget {
               startScreenButton(name: 'Not Adult'),
               startScreenButton(name: 'Neither Adult'),
 >>>>>>> a555e76 ([Task] #29 Adding Start Screen first draft)
+=======
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ResponsiveGridCol(
+            md: 6,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+              startScreenButton(name: 'Adult', image: 'lib/src/assets/icons/children.svg'),
+              startScreenButton(name: 'Children', image: 'lib/src/assets/icons/children.svg'),
+              startScreenButton(name: 'Infant', image: 'lib/src/assets/icons/newborn.svg'),
+>>>>>>> 237c277 ([TASK] #29 Added 2 icons, aligned button content)
             ])),
         ResponsiveGridCol(
           md: 6,
@@ -49,6 +68,7 @@ class startScreen extends StatelessWidget {
 
 class startScreenButton extends StatelessWidget {
   final String name;
+<<<<<<< HEAD
 <<<<<<< HEAD
   final String image;
   const startScreenButton({
@@ -85,11 +105,16 @@ class startScreenButton extends StatelessWidget {
             ],
           ),
 =======
+=======
+  final String image;
+>>>>>>> 237c277 ([TASK] #29 Added 2 icons, aligned button content)
   const startScreenButton({
     required this.name,
+    required this.image,
   });
   @override
   Widget build(BuildContext context) {
+<<<<<<< HEAD
     return Container(
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(75), color: Color(0xffeeeeee)),
@@ -100,6 +125,35 @@ class startScreenButton extends StatelessWidget {
         onPressed: null,
         child: Text(name),
 >>>>>>> a555e76 ([Task] #29 Adding Start Screen first draft)
+=======
+      return Container(
+        margin: EdgeInsets.only(bottom: 12.0),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size (800/MediaQuery.of(context).devicePixelRatio, 155/MediaQuery.of(context).devicePixelRatio),
+            primary: Color(0xffeeeeee),
+            onPrimary: Colors.black,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(75)),
+          ),
+          onPressed: (){ print (name);},
+          child: Row(
+            children: [
+              Expanded(
+                flex: 4,
+                child: SvgPicture.asset(image, height: 65,)),
+              Expanded(
+                flex: 5,
+                child: Text(
+                  name, 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25))),
+              Expanded(
+                flex: 4,
+                child: Container(), //Fynn bitte nicht töten, wir wissen nicht wie sonst :(
+                ),
+            ],
+          ),
+>>>>>>> 237c277 ([TASK] #29 Added 2 icons, aligned button content)
       ),
     );
   }
