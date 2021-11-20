16b0925d195178934e74acd76153f507c19bdb64
Anni <anni@rb-reinert.de>
2021-11-20 12:00:51 +0100

    [TASK] #29 new layout, added icon + demoScreen

---
+++ App/uke_mlab/lib/widgets/start_screen_button.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+//Widget for StartScreen Buttons
+//@param name Buttontext
+//@param optional image SVG asset path
+// ignore_for_file: camel_case_types, avoid_print
+
+class startScreenButton extends StatelessWidget {
+  final String name;
+  final String image;
+  const startScreenButton({
+    required this.name,
+    required this.image,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //Button style
+      margin: const EdgeInsets.fromLTRB(65, 0, 0, 12),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          fixedSize: Size(800 / MediaQuery.of(context).devicePixelRatio,
+              155 / MediaQuery.of(context).devicePixelRatio),
+          primary: const Color(0xffeeeeee),
+          onPrimary: Colors.black,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(75)),
+        ),
+        //Presets Loading
+        onPressed: () {
+          print(name);
+        },
+        //Button content
+        child: Row(
+          children: [
+            Flexible(
+                flex: 2,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset(
+                      image,
+                      height: 70,
+                    ),
+                  ],
+                )),
+            Flexible(
+              flex: 3,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        right:
+                            127), //TODO: Wie bekommen wir den Text ohne Padding gecentert im gesamt Button?
+                    child: Text(
+                      name,
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
