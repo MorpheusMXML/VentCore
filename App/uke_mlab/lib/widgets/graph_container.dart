339fdc019e99789c172bedaffd3a3ec7f33cf911
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-27 14:17:25 +0100

    [REFACTOR] Pass received data to Graph (Monitor --> GraphContainer --> Graph)

--- App/uke_mlab/lib/widgets/graph_container.dart
+++ App/uke_mlab/lib/widgets/graph_container.dart
@@ -1,26 +1,32 @@
-import 'dart:async';
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:uke_mlab/providers/mockup.dart';
 import 'package:uke_mlab/widgets/bell.dart';
 import 'package:uke_mlab/widgets/value_box.dart';
 import 'package:uke_mlab/widgets/graph.dart';
 
 class GraphContainer extends StatelessWidget {
-  const GraphContainer({Key? key}) : super(key: key);
+  final List<ChartData> data;
+
+  const GraphContainer({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       flex: 1,
       child: Row(
-        children: const [
+        children: [
           Flexible(
             flex: 1,
             child: Bell(),
           ),
           Flexible(
             flex: 6,
-            child: Graph(),
+            child: Graph(data: data),
           ),
           Flexible(
             flex: 2,
@@ -34,10 +40,4 @@ class GraphContainer extends StatelessWidget {
       ),
     );
   }
-}
-
-class ChartData {
-  ChartData(this.time, this.value);
-  final String time;
-  final double? value;
 }
