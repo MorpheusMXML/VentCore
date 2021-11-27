5119b9fd7dbcdea4ff0b2e68bac32206a934e8d9
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-27 14:12:00 +0100

    [TASK] #24 Add (no-database) random data, Binding and Controller for getX

--- App/uke_mlab/lib/providers/mockup.dart
+++ App/uke_mlab/lib/providers/mockup.dart
@@ -1,1 +1,50 @@
-//This directory is supposed to hold all the interactions that transact the data from outside the app.
+import 'dart:math';
+
+import 'package:get/get.dart';
+
+class ChartData {
+  ChartData(this.time, this.value);
+  final DateTime time;
+  final double value;
+}
+
+class MonitorBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(MonitorController());
+  }
+}
+
+class MonitorController extends GetxController {
+  int counter = 5;
+
+  var data = [
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0),
+    ChartData(DateTime.now(), 0)
+  ].obs;
+
+  var data2 = [ChartData(DateTime.now(), 0)].obs;
+
+  var data3 = [ChartData(DateTime.now(), 0)].obs;
+
+  Random random = Random();
+
+  updateData() {
+    data.add(ChartData(DateTime.now(), random.nextInt(100).toDouble()));
+    data.removeAt(0);
+  }
+}
