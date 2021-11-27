1bc0f46d5c5514626d661dd21c725f05c430ae6c
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-27 14:15:20 +0100

    [TASK] #22 #54 Access data via getX, update with random value on button press (TODO: Add timer to automaticall update every x milliseconds)

--- App/uke_mlab/lib/widgets/graph.dart
+++ App/uke_mlab/lib/widgets/graph.dart
@@ -1,39 +1,58 @@
 import 'dart:async';
-
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:uke_mlab/providers/mockup.dart';
 
 class Graph extends StatelessWidget {
-  const Graph({Key? key}) : super(key: key);
+  final List<ChartData> data;
+
+  const Graph({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final monitorController = Get.find<MonitorController>();
+    ChartSeriesController? _chartSeriesController;
+
     return Container(
       margin: const EdgeInsets.only(top: 30, bottom: 20),
-      child: SfCartesianChart(
-        // Initialize category axis
-        primaryXAxis: CategoryAxis(),
-        series: <ChartSeries>[
-          // Initialize line series
-          LineSeries<ChartData, String>(
-              dataSource: [
-                // Bind data source
-                ChartData('Jan', 35),
-                ChartData('Feb', 28),
-                ChartData('Mar', 34),
-                ChartData('Apr', 32),
-                ChartData('May', 40)
+      child: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            child: ElevatedButton(
+              onPressed: () {
+                _chartSeriesController?.updateDataSource(
+                  addedDataIndexes: <int>[data.length - 1],
+                  removedDataIndexes: <int>[0],
+                );
+                monitorController.updateData();
+              },
+              child: Text("update"),
+            ),
+          ),
+          Flexible(
+            flex: 4,
+            child: SfCartesianChart(
+              primaryXAxis: CategoryAxis(),
+              series: [
+                LineSeries(
+                    dataSource: data,
+                    onRendererCreated: (ChartSeriesController controller) {
+                      _chartSeriesController = controller;
+                    },
+                    xValueMapper: (ChartData data, _) => data.time,
+                    yValueMapper: (ChartData data, _) => data.value)
               ],
-              xValueMapper: (ChartData sales, _) => sales.time,
-              yValueMapper: (ChartData sales, _) => sales.value)
+            ),
+          ),
         ],
       ),
     );
   }
-}
-
-class ChartData {
-  ChartData(this.time, this.value);
-  final String time;
-  final double? value;
 }
