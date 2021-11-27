55f1efb504f5e5c5084eaab78640dacea586eea3
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-27 23:24:50 +0100

    [TASK] #28 Add tap detecting logic (conditional rendering)

--- App/uke_mlab/lib/widgets/graph_adder.dart
+++ App/uke_mlab/lib/widgets/graph_adder.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uke_mlab/providers/mockup.dart';
 
-class GraphAdder extends StatefulWidget {
-  const GraphAdder({Key? key}) : super(key: key);
+class GraphAdder extends StatelessWidget {
+  const GraphAdder({
+    Key? key,
+  }) : super(key: key);
 
-  @override
-  _GraphAdderState createState() => _GraphAdderState();
-}
-
-class _GraphAdderState extends State<GraphAdder> {
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => showGraphs(),
-      style: ElevatedButton.styleFrom(
-        primary: Colors.grey[800],
-        fixedSize: const Size(80, 80),
-        shape: const CircleBorder(),
-      ),
-      child: const Icon(Icons.add, size: 40),
-    );
+    return GetBuilder<MonitorController>(
+        init: MonitorController(),
+        builder: (controller) => getWidget(controller));
   }
 
-  showGraphs() {
-    setState(() {});
+  getWidget(controller) {
+    if (controller.isAddGraphTapped) {
+      return Text("popup");
+    } else {
+      return ElevatedButton(
+        onPressed: () => controller.invert(),
+        style: ElevatedButton.styleFrom(
+          primary: Colors.grey[800],
+          fixedSize: const Size(80, 80),
+          shape: const CircleBorder(),
+        ),
+        child: const Icon(Icons.add, size: 40),
+      );
+    }
   }
 }
