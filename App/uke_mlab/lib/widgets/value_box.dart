915370ace586b47e7e0209de84ac2c8933dc0c67
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-28 13:46:42 +0100

    [REFACTOR] #27 Remove unnecessary Containers & margins

--- App/uke_mlab/lib/widgets/value_box.dart
+++ App/uke_mlab/lib/widgets/value_box.dart
@@ -23,88 +23,82 @@ class ValueBox extends StatelessWidget {
 
     return ElevatedButton(
       style: ButtonStyle(
+        shape: MaterialStateProperty.all(const RoundedRectangleBorder()),
         backgroundColor: MaterialStateProperty.all(Color(backgroundColor)),
       ),
       onPressed: _showSnack,
-      child: Container(
-        margin: const EdgeInsets.only(top: 8, bottom: 4, left: 4, right: 4),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    width: double.maxFinite,
-                    margin: const EdgeInsets.only(left: 8, top: 8),
-                    child: Text(
-                      miniTitle,
-                      style: TextStyle(color: Color(textColor)),
-                      textAlign: TextAlign.left,
-                    ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: Text(
+                    miniTitle,
+                    style: TextStyle(color: Color(textColor)),
+                    textAlign: TextAlign.left,
                   ),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.only(right: 8, top: 8),
-                    width: double.maxFinite,
-                    child: Text(
-                      "120",
-                      style: TextStyle(
-                        // doesn't work here for some reason
-                        //overflow: TextOverflow.ellipsis,
-                        color: Color(textColor),
-                      ),
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
-                ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            Text(
-              value.toString(),
-              style: TextStyle(
-                color: Color(textColor),
-                fontSize: 80,
               ),
-            ),
-            Row(
-              children: [
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    width: double.maxFinite,
-                    margin: const EdgeInsets.only(left: 8),
-                    child: Text(
-                      "58",
-                      style: TextStyle(color: Color(textColor)),
-                      textAlign: TextAlign.left,
+              Flexible(
+                flex: 1,
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: Text(
+                    "120",
+                    style: TextStyle(
+                      // doesn't work here for some reason
+                      //overflow: TextOverflow.ellipsis,
+                      color: Color(textColor),
                     ),
+                    textAlign: TextAlign.right,
                   ),
                 ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    margin: const EdgeInsets.only(right: 8),
-                    width: double.maxFinite,
-                    child: Text(
-                      "1/ min.",
-                      style: TextStyle(
-                        // doesn't work here for some reason
-                        //overflow: TextOverflow.ellipsis,
-                        color: Color(textColor),
-                      ),
-                      textAlign: TextAlign.right,
-                    ),
+              ),
+            ],
+          ),
+          Text(
+            value.toString(),
+            style: TextStyle(
+              color: Color(textColor),
+              fontSize: 80,
+            ),
+          ),
+          Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: Text(
+                    "58",
+                    style: TextStyle(color: Color(textColor)),
+                    textAlign: TextAlign.left,
                   ),
                 ),
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ),
+              ),
+              Flexible(
+                flex: 1,
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: Text(
+                    "1/ min.",
+                    style: TextStyle(
+                      // doesn't work here for some reason
+                      //overflow: TextOverflow.ellipsis,
+                      color: Color(textColor),
+                    ),
+                    textAlign: TextAlign.right,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
