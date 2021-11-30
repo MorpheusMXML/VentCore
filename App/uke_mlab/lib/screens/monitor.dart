c7b6c031d5c90bf8f73d3a7f4255c2f69ee46649
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-11-30 11:05:23 +0100

    [TASK] #45 Remove Container StatusBar

--- App/uke_mlab/lib/screens/monitor.dart
+++ App/uke_mlab/lib/screens/monitor.dart
@@ -14,125 +14,111 @@ class Monitor extends StatelessWidget {
   Widget build(BuildContext context) {
     final monitorController = Get.find<MonitorController>();
 
-    return Container(
-      color: Theme.of(context).scaffoldBackgroundColor,
-      child: Column(
-        children: [
-          const Flexible(
-            child: StatusBar(),
-            flex: 1,
+    return Row(
+      children: [
+        Flexible(
+          flex: 4,
+          child: Column(
+            // Add condition here?
+            // If GraphAdder is tapped, render different children
+            children: [
+              GraphContainer(
+                  data: monitorController.data, color: Colors.yellow),
+              // The containers in the column are there for spacing
+              Container(height: 10),
+              GraphContainer(
+                  data: monitorController.data2, color: Colors.purple),
+              Container(height: 10),
+              GraphContainer(
+                  data: monitorController.data3, color: Colors.green),
+              Container(height: 10),
+              const GraphAdder(),
+            ],
           ),
-          Flexible(
-            flex: 9,
-            child: Row(
-              children: [
-                Flexible(
-                  flex: 4,
-                  child: Column(
-                    // Add condition here?
-                    // If GraphAdder is tapped, render different children
-                    children: [
-                      GraphContainer(
-                          data: monitorController.data, color: Colors.yellow),
-                      // The containers in the column are there for spacing
-                      Container(height: 10),
-                      GraphContainer(
-                          data: monitorController.data2, color: Colors.purple),
-                      Container(height: 10),
-                      GraphContainer(
-                          data: monitorController.data3, color: Colors.green),
-                      Container(height: 10),
-                      const GraphAdder(),
-                    ],
-                  ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Column(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: const [
+                    ValueTile(
+                      name: "NIDB",
+                      textColor: Color(0xFFDC362E),
+                      backgroundColor: Color(0xFF2A2831),
+                      value: 0,
+                    ),
+                    ValueTile(
+                        name: "Pulse",
+                        textColor: Color(0xFFFF00E4),
+                        backgroundColor: Color(0xFF2A2831),
+                        value: 0),
+                  ],
                 ),
-                Flexible(
-                  flex: 2,
-                  child: Column(
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Row(
-                          children: const [
-                            ValueTile(
-                              name: "NIDB",
-                              textColor: Color(0xFFDC362E),
-                              backgroundColor: Color(0xFF2A2831),
-                              value: 0,
-                            ),
-                            ValueTile(
-                                name: "Pulse",
-                                textColor: Color(0xFFFF00E4),
-                                backgroundColor: Color(0xFF2A2831),
-                                value: 0),
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Row(
-                          children: const [
-                            ValueTile(
-                              name: "MVe",
-                              textColor: Color(0xFF0CECDD),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                            ValueTile(
-                              name: "Breath. Freq.",
-                              textColor: Color(0xFF0CECDD),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        flex: 2,
-                        child: Row(
-                          children: const [
-                            ValueTile(
-                              name: "placeholder",
-                              textColor: Color(0xFFFFFFFF),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                            ValueTile(
-                              name: "placeholder",
-                              textColor: Color(0xFFFFFFFF),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                          ],
-                        ),
-                      ),
-                      Flexible(
-                        flex: 1,
-                        child: Row(
-                          children: const [
-                            ValueTile(
-                              name: "placeholder",
-                              textColor: Color(0xFFFFFFFF),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                            ValueTile(
-                              name: "placeholder",
-                              textColor: Color(0xFFFFFFFF),
-                              backgroundColor: Color(0xff2A2831),
-                              value: 0,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: const [
+                    ValueTile(
+                      name: "MVe",
+                      textColor: Color(0xFF0CECDD),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                    ValueTile(
+                      name: "Breath. Freq.",
+                      textColor: Color(0xFF0CECDD),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Row(
+                  children: const [
+                    ValueTile(
+                      name: "placeholder",
+                      textColor: Color(0xFFFFFFFF),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                    ValueTile(
+                      name: "placeholder",
+                      textColor: Color(0xFFFFFFFF),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: const [
+                    ValueTile(
+                      name: "placeholder",
+                      textColor: Color(0xFFFFFFFF),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                    ValueTile(
+                      name: "placeholder",
+                      textColor: Color(0xFFFFFFFF),
+                      backgroundColor: Color(0xff2A2831),
+                      value: 0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
