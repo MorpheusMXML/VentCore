fd14d3484cdfad702bbd63bb5811e42c4d3c14c7
Noah Scheld <noah.scheld@studium.uni-hamburg.de>
2021-12-01 20:49:55 +0100

    [REFACTOR] #50 created directory for setting widgets

---
+++ App/uke_mlab/lib/widgets/setting/setting.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:uke_mlab/widgets/setting_text.dart';
+
+class Setting extends StatelessWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          flex: 1,
+          child:
+              ElevatedButton(onPressed: () {}, child: const Icon(Icons.remove)),
+        ),
+        const Flexible(flex: 2, child: SettingText()),
+        Flexible(
+          flex: 1,
+          child: ElevatedButton(onPressed: () {}, child: const Icon(Icons.add)),
+        ),
+      ],
+    );
+  }
+}
---
+++ App/uke_mlab/lib/widgets/setting/setting_tile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:uke_mlab/widgets/setting.dart';
+
+class SettingTile extends StatelessWidget {
+  const SettingTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          child: const Text("IPPV"),
+          onPressed: () {},
+        ),
+        const Setting(),
+        const Setting(),
+        const Setting(),
+      ],
+    );
+  }
+}
