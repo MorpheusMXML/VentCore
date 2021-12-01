86f396a287a4e0e2327fd17b700df57efc02ac3a
Noah Scheld <noah.scheld@studium.uni-hamburg.de>
2021-12-01 21:19:21 +0100

    [REFACTOR] #48 info widgets got their own directory

---
+++ App/uke_mlab/lib/widgets/info/info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Info extends StatelessWidget {
+  const Info({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(8),
+        color: Colors.amber,
+      ),
+    );
+  }
+}
