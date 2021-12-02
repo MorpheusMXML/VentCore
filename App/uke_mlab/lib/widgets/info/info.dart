bb4e053ca522f7e1ea0a330c81d77f335bf3a4fd
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-02 11:27:59 +0100

    TASK #49 Add Info widget (takes type, value, unit from InfoTile and passes them to InfoText)

--- App/uke_mlab/lib/widgets/info/info.dart
+++ App/uke_mlab/lib/widgets/info/info.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:uke_mlab/widgets/info/info_text.dart';
 
 class Info extends StatelessWidget {
-  const Info({Key? key}) : super(key: key);
+  final String type;
+  final double value;
+  final String unit;
+
+  const Info({
+    Key? key,
+    required this.type,
+    required this.value,
+    required this.unit,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
-        margin: const EdgeInsets.all(8),
-        color: Colors.amber,
-      ),
+          decoration: const BoxDecoration(
+            color: Color(0xFF2A2831),
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          margin: const EdgeInsets.all(8),
+          child: Container(
+              margin: const EdgeInsets.only(left: 12, right: 12),
+              child: InfoText(type: type, value: value, unit: unit))),
     );
   }
 }
