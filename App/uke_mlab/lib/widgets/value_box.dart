fcd32a4f7f163354eb90f1e7d8eff10866897b92
9menk <fynn-ole.menk@studium.uni-hamburg.de>
2021-12-02 02:54:07 +0100

    [TASK] Value tile and Value box are more fletched out and received some more...

---
+++ App/uke_mlab/lib/widgets/value_box.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class ValueBox extends StatelessWidget {
+  final Color textColor;
+  final int value;
+  final Color backgroundColor;
+  final String miniTitle;
+
+  const ValueBox({
+    Key? key,
+    required this.textColor,
+    required this.value,
+    // to be changed
+    this.miniTitle = "PP",
+    required this.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    void _showSnack() => ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text("Button Tapped")));
+
+    return AspectRatio(
+      aspectRatio: 1,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all(const RoundedRectangleBorder()),
+          backgroundColor: MaterialStateProperty.all(backgroundColor),
+        ),
+        onPressed: _showSnack,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  miniTitle,
+                  style: TextStyle(color: textColor),
+                ),
+                Text(
+                  // to be changed
+                  "120",
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              value.toString(),
+              style: TextStyle(
+                color: textColor,
+                fontSize: 50,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  // to be changed
+                  "58",
+                  style: TextStyle(color: textColor),
+                  textAlign: TextAlign.left,
+                ),
+                Text(
+                  // to be changed
+                  "1/ min.",
+                  style: TextStyle(
+                    color: textColor,
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ App/uke_mlab/lib/widgets/value_tile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:uke_mlab/widgets/value_box.dart';
+
+class ValueTile extends StatelessWidget {
+  final Color textColor;
+  final Color backgroundColor;
+  final int value;
+  final String name;
+
+  const ValueTile({
+    Key? key,
+    required this.textColor,
+    required this.name,
+    required this.backgroundColor,
+    required this.value,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: const Color(0xFF25232A),
+        margin: const EdgeInsets.only(right: 8, bottom: 8),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Container(
+                margin: const EdgeInsets.only(top: 4, bottom: 4),
+                child: Text(
+                  name,
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    color: textColor,
+                    fontSize: 20,
+                    // temp fix for long names
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              // cant decrease --> pixel overflow
+              flex: 3,
+              child: ValueBox(
+                backgroundColor: backgroundColor,
+                textColor: textColor,
+                value: value,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
