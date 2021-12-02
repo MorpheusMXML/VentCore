c9675aed597fbce64003cc3c86d66918e41384bf
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-02 13:09:48 +0100

    [TASK] #49 Pass data to Info widget

--- App/uke_mlab/lib/widgets/info/info_tile.dart
+++ App/uke_mlab/lib/widgets/info/info_tile.dart
@@ -2,20 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:uke_mlab/widgets/info/info.dart';
 
 class InfoTile extends StatelessWidget {
-  const InfoTile({Key? key}) : super(key: key);
+  final List<Map<String, Object>> data;
+  const InfoTile({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: const [
-          Info(type: "pPeak", value: 50.12, unit: "mBar"),
-          Info(type: "pPlat", value: 4.58, unit: "mBar"),
-          Info(type: "pMean", value: 16.58, unit: "mBar"),
-          Info(type: "MV", value: 7.2, unit: "l/min"),
-        ],
-      ),
-    );
+    return Info(data: data);
   }
 }
