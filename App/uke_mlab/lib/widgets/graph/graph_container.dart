9eb8f45ec0ea52a8af7afcdeff93d3b9f932ffa1
Noah Scheld <noah.scheld@studium.uni-hamburg.de>
2021-12-04 19:03:09 +0100

    [REBASE] fix merge conflicts

--- App/uke_mlab/lib/widgets/graph/graph_container.dart
+++ App/uke_mlab/lib/widgets/graph/graph_container.dart
@@ -6,49 +6,43 @@ import 'package:uke_mlab/widgets/value_box.dart';
 import 'package:uke_mlab/widgets/graph/graph.dart';
 
 class GraphContainer extends StatelessWidget {
-  final List<ChartData> data;
-  final Color color;
-  final String type;
+  final List<Map<String, Object>> data;
 
   const GraphContainer({
     Key? key,
-    required this.type,
     required this.data,
-    required this.color,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      flex: 2,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                const GraphNotification(),
-                Container(width: 10),
-                Expanded(
-                  child: Graph(
-                    type: type,
-                    data: data,
-                    color: color,
+    return Expanded(
+      child: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.only(bottom: 8),
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(maxHeight: 150),
+              child: Row(
+                children: [
+                  const GraphNotification(),
+                  Expanded(
+                    child: Graph(
+                      type: data[index]["type"] as String,
+                      data: data[index]["data"] as List<ChartData>,
+                      color: data[index]["color"] as Color,
+                    ),
                   ),
-                ),
-                Container(width: 10),
-                Obx(
-                  () => ValueBox(
-                    value: data[data.length - 2].value,
-                    textColor: color,
-                    backgroundColor: const Color(0xff2A2831),
+                  ValueBox(
+                    value: data[index]["data"] as List<ChartData>,
+                    textColor: data[index]["color"] as Color,
+                    backgroundColor: const Color(0xFF2A2831),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          )
-        ],
+          );
+        },
       ),
     );
   }
