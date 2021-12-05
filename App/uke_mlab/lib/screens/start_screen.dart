14ac79b2ceec1bf016fe9a8dbe69e76b5b6ccb02
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-05 12:42:06 +0100

    [TASK] #60 #45 Add Appbar, do not display any category in Appbar of starting screen

--- App/uke_mlab/lib/screens/start_screen.dart
+++ App/uke_mlab/lib/screens/start_screen.dart
@@ -6,58 +6,62 @@ import 'package:uke_mlab/widgets/start_screen/name_field.dart';
 import 'package:uke_mlab/widgets/start_screen/slider.dart';
 import 'package:uke_mlab/widgets/start_screen/start_screen_button.dart';
 import 'package:uke_mlab/widgets/start_screen/start_screen_skip.dart';
+import 'package:uke_mlab/widgets/statusbar.dart';
 
 class startScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Flexible(
-        flex: 5,
-        child: Row(
-          children: [
-            Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  startScreenButton(
-                      name: 'Adult', image: 'assets/icons/adults2.svg'),
-                  startScreenButton(
-                      name: 'Children', image: 'assets/icons/children.svg'),
-                  startScreenButton(
-                      name: 'Infant', image: 'assets/icons/newborn.svg'),
-                ]),
-          ],
-        ),
-      ),
-      Flexible(
-        flex:
-            3, //von 4 auf 3 damit Patientencontainer wieder auf eine höhe mit Buttons
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            //Ganzer Container auf einer Höhe mit Buttons darunter
-            Align(
-              alignment: Alignment.centerRight,
-              //Fetter Container
-              child: Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 65, 12),
-                color: Color(0xff25232a),
-                height: 320 / MediaQuery.of(context).devicePixelRatio,
-                width: 830 / MediaQuery.of(context).devicePixelRatio,
-                child: Column(
-                  //TODO: implement Logic in these 3 Widgets so that we can work with this data in the following
+    return Scaffold(
+      appBar: AppBar(title: const StatusBar(category: "")),
+      body: Row(children: [
+        Flexible(
+          flex: 5,
+          child: Row(
+            children: [
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    nameField(),
-                    startScreenSlider(name: "Weight"),
-                    startScreenSlider(name: "Height"),
-                  ],
+                    StartScreenButton(
+                        name: 'Adult', image: 'assets/icons/adults2.svg'),
+                    StartScreenButton(
+                        name: 'Children', image: 'assets/icons/children.svg'),
+                    StartScreenButton(
+                        name: 'Infant', image: 'assets/icons/newborn.svg'),
+                  ]),
+            ],
+          ),
+        ),
+        Flexible(
+          flex:
+              3, //von 4 auf 3 damit Patientencontainer wieder auf eine höhe mit Buttons
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              //Ganzer Container auf einer Höhe mit Buttons darunter
+              Align(
+                alignment: Alignment.centerRight,
+                //Fetter Container
+                child: Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 65, 12),
+                  color: Color(0xff25232a),
+                  height: 320 / MediaQuery.of(context).devicePixelRatio,
+                  width: 830 / MediaQuery.of(context).devicePixelRatio,
+                  child: Column(
+                    //TODO: implement Logic in these 3 Widgets so that we can work with this data in the following
+                    children: const [
+                      nameField(),
+                      startScreenSlider(name: "Weight"),
+                      startScreenSlider(name: "Height"),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            //TODO: add a "Continue" Button so that if content is changed in "Fetter Container" the button appears and only by pressing continue we will provide/save the configuration
-            StartScreenSkip()
-          ],
+              //TODO: add a "Continue" Button so that if content is changed in "Fetter Container" the button appears and only by pressing continue we will provide/save the configuration
+              StartScreenSkip()
+            ],
+          ),
         ),
-      ),
-    ]);
+      ]),
+    );
   }
 }
