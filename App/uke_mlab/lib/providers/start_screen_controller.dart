7310fc9599e7160a1a6edba82518a477ed5f6de2
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-06 15:39:23 +0100

    [TASK/BUGFIX] #60 Set default value for patient, selected button displayed in different color

--- App/uke_mlab/lib/providers/start_screen_controller.dart
+++ App/uke_mlab/lib/providers/start_screen_controller.dart
@@ -9,15 +9,15 @@ class StartScreenBinding extends Bindings {
 
 class StartScreenController extends GetxController {
   RxString selectedString = "".obs;
-  RxDouble weightValue = 20.0.obs;
-  RxDouble heightValue = 20.0.obs;
+  RxDouble weightValue = 75.0.obs;
+  RxDouble heightValue = 175.0.obs;
 
   void settingsButton(String name) {
     switch (name) {
       case "Child":
         selectedString.value = name;
-        weightValue.value = 47.0;
-        heightValue.value = 156.0;
+        weightValue.value = 45.0;
+        heightValue.value = 155.0;
         break;
       case "Infant":
         selectedString.value = name;
@@ -26,9 +26,11 @@ class StartScreenController extends GetxController {
         break;
       default:
         selectedString.value = name;
-        weightValue.value = 77.0;
-        heightValue.value = 172.0;
+        weightValue.value = 75.0;
+        heightValue.value = 175.0;
     }
+
+    update();
   }
 
   setValue(newValue, name) {
