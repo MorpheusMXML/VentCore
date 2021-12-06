c265324df94d318365789bc1ad11446dadb27ffd
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-06 17:55:37 +0100

    [TASK] #51 #60 Use ListView map for IPPV components (easily expandable), Finish State Management logic (i.e. display, increment/decrement)

--- App/uke_mlab/lib/widgets/setting/setting.dart
+++ App/uke_mlab/lib/widgets/setting/setting.dart
@@ -1,60 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:uke_mlab/providers/mockup.dart';
 import 'package:uke_mlab/widgets/setting/setting_text.dart';
 
 class Setting extends StatelessWidget {
-  final MonitorController controller;
   final String name;
   final String rate;
-  const Setting(
-      {Key? key,
-      required this.controller,
-      required this.name,
-      required this.rate})
+  const Setting({Key? key, required this.name, required this.rate})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Flexible(
-          flex: 1,
-          child: ElevatedButton(
-            onPressed: () {},
-            child: const Icon(Icons.remove),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
+    final monitorController = Get.find<MonitorController>();
+
+    return Container(
+      color: const Color(0xFF2A2831),
+      child: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            child: ElevatedButton(
+              onPressed: () {
+                monitorController.decrement(name);
+              },
+              child: const Icon(Icons.remove),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                primary: const Color(0xFF5AC8FA),
               ),
-              primary: const Color(0xFF5AC8FA),
             ),
           ),
-        ),
-        Flexible(
-          flex: 2,
-          child: SettingText(
-            controller: controller,
-            name: name,
-            rate: rate,
-          ),
-        ),
-        Flexible(
-          flex: 1,
-          child: ElevatedButton(
-            onPressed: () {
-              // TODO: find fix to only alter one setting
-              controller.increment(name);
-            },
-            child: const Icon(Icons.add),
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              primary: const Color(0xFF5AC8FA),
+          Flexible(
+            flex: 2,
+            child: SettingText(
+              name: name,
+              rate: rate,
             ),
           ),
-        ),
-      ],
+          Flexible(
+            flex: 1,
+            child: ElevatedButton(
+              onPressed: () {
+                monitorController.increment(name);
+              },
+              child: const Icon(Icons.add),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                primary: const Color(0xFF5AC8FA),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
