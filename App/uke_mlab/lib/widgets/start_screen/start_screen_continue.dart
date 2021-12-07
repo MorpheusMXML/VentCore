48a90220c36230f347c0cf26e4348aa87626b37d
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-07 13:10:25 +0100

    [REFACTOR] #29 Refactor

--- App/uke_mlab/lib/widgets/start_screen/start_screen_continue.dart
+++ App/uke_mlab/lib/widgets/start_screen/start_screen_continue.dart
@@ -15,25 +15,21 @@ class StartScreenContinue extends StatelessWidget {
   }
 
   getWidget(controller) {
-    return Container(
-      margin: const EdgeInsets.fromLTRB(0, 0, 65, 12),
-      child: Obx(
-        () => ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            onSurface: const Color(0xffeeeeee),
-            primary: const Color(0xffeeeeee),
-            onPrimary: Colors.black,
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(75)),
-          ),
-          onPressed: controller.selectedString.toString() != ""
-              ? () {
-                  Get.toNamed('/monitor',
-                      arguments: [controller.selectedString]);
-                }
-              : null,
-          child: const Text('Continue', style: TextStyle(fontSize: 20)),
+    return Obx(
+      () => ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          onSurface: const Color(0xffeeeeee),
+          primary: const Color(0xffeeeeee),
+          onPrimary: Colors.black,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(75)),
         ),
+        onPressed: controller.selectedString.toString() != ""
+            ? () {
+                Get.toNamed('/monitor', arguments: [controller.selectedString.value]);
+              }
+            : null,
+        child: const Text('Continue', style: TextStyle(fontSize: 20)),
       ),
     );
   }
