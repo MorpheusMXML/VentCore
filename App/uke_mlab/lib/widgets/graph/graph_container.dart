33b5ea4c3281896c366135b55556eb4cb6a0d4f2
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-07 14:38:14 +0100

    [TASK] #62 Add logic for Alarm States in Graphs

--- App/uke_mlab/lib/widgets/graph/graph_container.dart
+++ App/uke_mlab/lib/widgets/graph/graph_container.dart
@@ -16,40 +16,51 @@ class GraphContainer extends StatelessWidget {
     List<Map<String, Object>> dataRef = monitorController.initialGraphs;
 
     return Expanded(
-      child: Obx(() => ListView.builder(
-            itemCount: dataRef.length,
-            itemBuilder: (BuildContext context, int index) {
-              Map<String, Object> graphRef = dataRef[index];
-              Map<String, Object> typeRef =
-                  graphRef["type"] as Map<String, Object>;
+      child: Obx(
+        () => ListView.builder(
+          itemCount: dataRef.length,
+          itemBuilder: (BuildContext context, int index) {
+            Map<String, Object> graphRef = dataRef[index];
+            Map<String, Object> typeRef =
+                graphRef["type"] as Map<String, Object>;
 
-              return Container(
-                margin: const EdgeInsets.only(bottom: 8),
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxHeight: 150),
-                  child: Row(
-                    children: [
-                      GraphNotification(type: typeRef),
-                      Container(width: 8),
-                      Expanded(
-                        child: Graph(
-                          type: typeRef,
-                          data: graphRef["data"] as List<ChartDataMockup>,
-                          color: graphRef["color"] as Color,
-                        ),
-                      ),
-                      Container(width: 8),
-                      ValueBox(
-                        value: graphRef["data"] as List<ChartDataMockup>,
-                        textColor: graphRef["color"] as Color,
-                        backgroundColor: const Color(0xFF2A2831),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          )),
+            return getWidget(monitorController, typeRef, graphRef);
+          },
+        ),
+      ),
     );
+  }
+
+  getWidget(MonitorController controller, Map<String, Object> typeRef,
+      Map<String, Object> graphRef) {
+    if (controller.isInAlarmState) {
+      return const Text("Alarm");
+    } else {
+      Container(
+        margin: const EdgeInsets.only(bottom: 8),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 150),
+          child: Row(
+            children: [
+              GraphNotification(type: typeRef),
+              Container(width: 8),
+              Expanded(
+                child: Graph(
+                  type: typeRef,
+                  data: graphRef["data"] as List<ChartDataMockup>,
+                  color: graphRef["color"] as Color,
+                ),
+              ),
+              Container(width: 8),
+              ValueBox(
+                value: graphRef["data"] as List<ChartDataMockup>,
+                textColor: graphRef["color"] as Color,
+                backgroundColor: const Color(0xFF2A2831),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
