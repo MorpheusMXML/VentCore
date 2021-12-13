6acb26871d3a77c72feff120fb56d2baab8b2bc9
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-13 09:05:38 +0100

    [TASK/BUGFIX] #74 Manage Timer in Controller class (Bug finally FIXED!)

--- App/uke_mlab/lib/widgets/graph/graph_notification.dart
+++ App/uke_mlab/lib/widgets/graph/graph_notification.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:uke_mlab/providers/mockup.dart';
 
 class GraphNotification extends StatelessWidget {
-  final Map<String, Object> type;
+  final Map<String, Object?> graphData;
 
   const GraphNotification({
     Key? key,
-    required this.type,
+    required this.graphData,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final monitorController = Get.find<MonitorController>();
-
     return ElevatedButton(
-      onPressed: () => monitorController.invertMuted(type["id"] as String),
+      onPressed: () => (graphData["muted"] as RxBool).value =
+          !(graphData["muted"] as RxBool).value,
       style: ElevatedButton.styleFrom(
         primary: Colors.grey[800],
         fixedSize: const Size(80, 80),
         shape: const CircleBorder(),
       ),
-      child: getWidget(monitorController),
+      child: getWidget(),
     );
   }
 
-  getWidget(MonitorController monitorController) {
+  getWidget() {
     return Obx(
-      () => (monitorController.muted[type["id"]] as RxBool).value
+      () => (graphData["muted"] as RxBool).value
           ? const Icon(Icons.notifications_off, size: 40)
           : const Icon(Icons.notifications, size: 40),
     );
