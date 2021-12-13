3428fa025f3230cd53b4f3d37eae347a7cde5e32
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-13 09:03:44 +0100

    [TASK] #62 Add logic for displaying visual alarm around the graph container

--- App/uke_mlab/lib/widgets/graph/graph_container.dart
+++ App/uke_mlab/lib/widgets/graph/graph_container.dart
@@ -7,7 +7,7 @@ import 'package:uke_mlab/widgets/value_box/value_box.dart';
 import 'package:uke_mlab/widgets/graph/graph.dart';
 
 class GraphContainer extends StatelessWidget {
-  final Map<String, Object> graphData;
+  final Map<String, Object?> graphData;
 
   const GraphContainer({
     Key? key,
@@ -16,41 +16,51 @@ class GraphContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //if ((graphRef["alarm"] as RxBool).value) {
-    //  return const Text("Alarm");
-    //} else {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 8),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxHeight: 150),
-        child: Row(
-          children: [
-            GraphNotification(type: graphData["type"] as Map<String, Object>),
-            Container(width: 8),
-            Expanded(
-              child: (graphData["type"] as Map<String, Object>)["id"] == "NIBD"
-                  ? HistoryGraph(
-                      color: Colors.red,
-                      data: graphData["data"] as List<NIBDdata>)
-                  : Graph(
-                      type: graphData["type"] as Map<String, Object>,
-                      data: graphData["data"] as List<ChartDataMockup>,
-                      color: graphData["color"] as Color,
-                    ),
+    return Obx(
+      () => ((graphData["alarm"] as RxBool).value)
+          ? Container(
+              color: Colors.red,
+              padding: const EdgeInsets.all(12),
+              child: Container(
+                margin: const EdgeInsets.only(bottom: 8),
+                child: getGraphRow(),
+              ),
+            )
+          : Container(
+              margin: const EdgeInsets.only(bottom: 8),
+              child: getGraphRow(),
             ),
-            Container(width: 8),
-            (graphData["type"] as Map<String, Object>)["id"] == "NIBD"
-                ? Container()
-                : ValueBox(
-                    value: graphData["data"] as List<ChartDataMockup>,
-                    miniTitle: (graphData["type"]
-                        as Map<String, Object>)["abbr"] as String,
-                    textColor: graphData["color"] as Color,
-                    backgroundColor: const Color(0xFF2A2831),
-                    withModel: false,
+    );
+  }
+
+  getGraphRow() {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(maxHeight: 150),
+      child: Row(
+        children: [
+          GraphNotification(graphData: graphData),
+          Container(width: 8),
+          Expanded(
+            child: (graphData["type"] as Map<String, Object>)["id"] == "NIBD"
+                ? HistoryGraph(
+                    color: Colors.red,
+                    data: graphData["data"] as List<NIBDdata>)
+                : Graph(
+                    graphData: graphData,
                   ),
-          ],
-        ),
+          ),
+          Container(width: 8),
+          (graphData["type"] as Map<String, Object>)["id"] == "NIBD"
+              ? Container()
+              : ValueBox(
+                  value: graphData["data"] as List<ChartDataMockup>,
+                  miniTitle: (graphData["type"] as Map<String, Object>)["abbr"]
+                      as String,
+                  textColor: graphData["color"] as Color,
+                  backgroundColor: const Color(0xFF2A2831),
+                  withModel: false,
+                ),
+        ],
       ),
     );
   }
