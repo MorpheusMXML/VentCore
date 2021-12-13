e5ff8da908dcc9b5cd10af4780baea0c4da8599d
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-13 09:05:04 +0100

    [TASK/BUGFIX] #74 Use Obx instead of GetBuilder for conditional rendering

--- App/uke_mlab/lib/widgets/start_screen/start_screen_popup.dart
+++ App/uke_mlab/lib/widgets/start_screen/start_screen_popup.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uke_mlab/providers/start_screen_controller.dart';
 import 'package:uke_mlab/widgets/start_screen/slider_tile.dart';
-
 import 'name_field.dart';
 
 class StartScreenPopup extends StatelessWidget {
@@ -12,30 +11,27 @@ class StartScreenPopup extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<StartScreenController>(
-        init: StartScreenController(),
-        builder: (controller) => getWidget(controller, context));
-  }
+    final startScreenController = Get.find<StartScreenController>();
 
-  getWidget(StartScreenController controller, BuildContext context) {
-    if (controller.isPopupVisible) {
-      return Container(
-        margin: const EdgeInsets.fromLTRB(0, 22, 65, 12),
-        color: const Color(0xff25232a),
-        child: Column(
-          //TODO: implement Logic in these 3 Widgets so that we can work with this data in the following
-          children: [
-            const nameField(),
-            SliderTile(name: "Weight", value: controller.weightValue),
-            SliderTile(name: "Height", value: controller.heightValue),
-          ],
-        ),
-      );
-    } else {
-      return Container(
-        height: 188,
-        margin: const EdgeInsets.fromLTRB(0, 0, 65, 12),
-      );
-    }
+    return Obx(
+      () => startScreenController.isPopupVisible.value
+          ? Container(
+              margin: const EdgeInsets.fromLTRB(0, 22, 65, 12),
+              color: const Color(0xff25232a),
+              child: Column(
+                children: [
+                  const nameField(),
+                  SliderTile(
+                      name: "Weight", value: startScreenController.weightValue),
+                  SliderTile(
+                      name: "Height", value: startScreenController.heightValue),
+                ],
+              ),
+            )
+          : Container(
+              height: 188,
+              margin: const EdgeInsets.fromLTRB(0, 0, 65, 12),
+            ),
+    );
   }
 }
