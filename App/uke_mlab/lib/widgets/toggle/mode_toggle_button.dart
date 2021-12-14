b298d45532d24bb801c7f55abc8465d931b3a9c2
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-14 17:55:09 +0100

    [TASK] #64 Create Controller for ToggleButton (make widget stateless, state management via Controller)

--- App/uke_mlab/lib/widgets/toggle/mode_toggle_button.dart
+++ App/uke_mlab/lib/widgets/toggle/mode_toggle_button.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:uke_mlab/providers/toggle_controller.dart';
 
-class ModeToggleButton extends StatefulWidget {
-  const ModeToggleButton({Key? key}) : super(key: key);
+class ModeToggleButton extends StatelessWidget {
+  ModeToggleButton({Key? key}) : super(key: key);
 
-  @override
-  _ModeToggleButtonState createState() => _ModeToggleButtonState();
-}
-
-class _ModeToggleButtonState extends State<ModeToggleButton> {
-  final List<bool> isSelected = [true, false, false];
+  final toggleController = Get.find<ToggleController>();
   final Map<String, String> iconLocationMap = {
     'Monitoring': 'assets/icons/monitoring.svg',
     'Ventilation': 'assets/icons/VentilatorIcon.svg',
     'Defibrillator': 'assets/icons/AED.svg',
   };
+
+  // eventually move to theme class
   final Color background = const Color(0xFF2A2831);
   final Color inactiveIcon = const Color(0xFF79747E);
   final Color active = const Color(0xFF5D5FEF);
@@ -22,58 +21,50 @@ class _ModeToggleButtonState extends State<ModeToggleButton> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        heightFactor: 2,
-        child: ToggleButtons(
+      heightFactor: 2,
+      child: Obx(
+        () => ToggleButtons(
           borderColor: Colors.grey[800],
           borderWidth: 0.5,
           selectedBorderColor: Colors.white,
           borderRadius: const BorderRadius.all(Radius.circular(25)),
           children: <Widget>[
-            Container(
-                decoration: BoxDecoration(color: isSelected[0] ? active : background),
-                width: 100,
-                child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: SvgPicture.asset(
-                      iconLocationMap['Monitoring'].toString(),
-                      height: 40.0,
-                      color: isSelected[0] ? Colors.white : inactiveIcon,
-                    ))),
-            Container(
-                decoration: BoxDecoration(color: isSelected[1] ? active : background),
-                width: 100,
-                child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: SvgPicture.asset(
-                      iconLocationMap['Ventilation'].toString(),
-                      height: 40.0,
-                      color: isSelected[1] ? Colors.white : inactiveIcon,
-                    ))),
-            Container(
-                decoration: BoxDecoration(color: isSelected[2] ? active : background),
-                width: 100,
-                child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: SvgPicture.asset(
-                      iconLocationMap['Defibrillator'].toString(),
-                      height: 40.0,
-                      color: isSelected[2] ? Colors.white : inactiveIcon,
-                    ))),
+            getToggleButton(
+              0,
+              iconLocationMap['Monitoring'].toString(),
+            ),
+            getToggleButton(
+              1,
+              iconLocationMap['Ventilation'].toString(),
+            ),
+            getToggleButton(
+              2,
+              iconLocationMap['Defibrillator'].toString(),
+            ),
           ],
           onPressed: (int index) {
-            //TODO: Implement Behaviour that should be called upon ToggleButtonPress
-
-            setState(() {
-              for (int buttonIndex = 0; buttonIndex < isSelected.length; buttonIndex++) {
-                if (buttonIndex == index) {
-                  isSelected[buttonIndex] = true;
-                } else {
-                  isSelected[buttonIndex] = false;
-                }
-              }
-            });
+            toggleController.setSelected(index);
           },
-          isSelected: isSelected,
-        ));
+          isSelected: toggleController.isSelected,
+        ),
+      ),
+    );
+  }
+
+  getToggleButton(int index, String path) {
+    return Container(
+      decoration: BoxDecoration(
+          color: toggleController.isSelected[index] ? active : background),
+      width: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: SvgPicture.asset(
+          path,
+          height: 40.0,
+          color:
+              toggleController.isSelected[index] ? Colors.white : inactiveIcon,
+        ),
+      ),
+    );
   }
 }
