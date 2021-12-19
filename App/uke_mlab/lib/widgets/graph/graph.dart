aacd8ed751b601d14fbc23ef26a090fd568bf650
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-19 18:36:23 +0100

    [TASK] #87 Make .withModel constructor (Graph, ValueBox, ValueTile) default constructor

--- App/uke_mlab/lib/widgets/graph/graph.dart
+++ App/uke_mlab/lib/widgets/graph/graph.dart
@@ -3,56 +3,22 @@ import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:uke_mlab/models/enums.dart';
 import 'package:uke_mlab/models/model.dart';
-import 'package:uke_mlab/providers/mockup.dart';
 
 class Graph extends StatelessWidget {
-  late Map<String, Object?> graphData;
-  late sensorEnum sensor;
-  ChartSeriesController? chartController;
+  final sensorEnum sensor;
 
-  late final bool withModel;
-
-  Graph({
-    Key? key,
-    required this.graphData,
-    this.withModel = false,
-  }) : super(key: key);
-
-  Graph.withModel({
+  const Graph({
     Key? key,
     required this.sensor,
-    this.withModel = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (withModel) {
-      DataModel dataModel = Get.find<DataModel>(tag: sensor.toString());
-      return Obx(
-        () => SfCartesianChart(
-          backgroundColor: Theme.of(context).cardColor,
-          primaryYAxis: NumericAxis(
-            majorGridLines:
-                MajorGridLines(width: 1, color: Theme.of(context).shadowColor),
-          ),
-          primaryXAxis: NumericAxis(
-            majorGridLines:
-                MajorGridLines(width: 1, color: Theme.of(context).shadowColor),
-          ),
-          series: [
-            SplineSeries(
-                color: dataModel.color,
-                dataSource: dataModel.graphData.value,
-                onRendererCreated: (ChartSeriesController controller) {
-                  chartController = controller;
-                },
-                xValueMapper: (ChartData data, _) => data.counter,
-                yValueMapper: (ChartData data, _) => data.value)
-          ],
-        ),
-      );
-    } else {
-      return SfCartesianChart(
+    ChartSeriesController? chartController;
+
+    DataModel dataModel = Get.find<DataModel>(tag: sensor.toString());
+    return Obx(
+      () => SfCartesianChart(
         backgroundColor: Theme.of(context).cardColor,
         primaryYAxis: NumericAxis(
           majorGridLines:
@@ -64,15 +30,15 @@ class Graph extends StatelessWidget {
         ),
         series: [
           SplineSeries(
-              color: graphData["color"] as Color,
-              dataSource: graphData["data"] as List<ChartDataMockup>,
+              color: dataModel.color,
+              dataSource: dataModel.graphData.value,
               onRendererCreated: (ChartSeriesController controller) {
-                graphData["controller"] = controller;
+                chartController = controller;
               },
-              xValueMapper: (ChartDataMockup data, _) => data.counter,
-              yValueMapper: (ChartDataMockup data, _) => data.value)
+              xValueMapper: (ChartData data, _) => data.counter,
+              yValueMapper: (ChartData data, _) => data.value)
         ],
-      );
-    }
+      ),
+    );
   }
 }
