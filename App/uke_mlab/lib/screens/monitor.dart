48c317827a646e62e81d4c93efd43cd2fce4108e
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-22 00:35:21 +0100

    [TASK] #64 #90 Move Scaffold up to main (less code), Add Ventilation toggle view

--- App/uke_mlab/lib/screens/monitor.dart
+++ App/uke_mlab/lib/screens/monitor.dart
@@ -10,7 +10,6 @@ import 'package:uke_mlab/widgets/graph/graph_container.dart';
 import 'package:uke_mlab/widgets/info/info_tile.dart';
 import 'package:uke_mlab/widgets/toggle/mode_toggle_button.dart';
 import 'package:uke_mlab/widgets/setting/setting_tile.dart';
-import 'package:uke_mlab/widgets/statusbar/statusbar.dart';
 import 'package:uke_mlab/widgets/value_box/value_tile.dart';
 import 'package:uke_mlab/widgets/graph/graph_adder.dart';
 
@@ -36,33 +35,30 @@ class Monitor extends StatelessWidget {
   Widget build(BuildContext context) {
     monitorController.activateTimer();
 
-    return Scaffold(
-        appBar: AppBar(title: StatusBar(category: Get.arguments[0])),
-        body: Container(
-            margin: const EdgeInsets.only(left: 12, right: 12, top: 12),
-            child: Obx(() => getToggledScreen())));
-  }
-
-  Widget getToggledScreen() {
-    if (toggleController.isSelected[1]) {
-      // Ventilation
-      return Row(children: [
-        Flexible(flex: 4, child: getGraphView()),
-        Flexible(flex: 2, child: ModeToggleButton())
-      ]);
-    } else if (toggleController.isSelected[2]) {
-      // Defibrillation
-      return Row(children: [
-        Flexible(flex: 4, child: getGraphView()),
-        Flexible(flex: 2, child: ModeToggleButton())
-      ]);
-    } else {
-      // Monitoring
-      return Row(children: [
-        Flexible(flex: 4, child: getGraphView()),
-        Flexible(flex: 2, child: getToggleView())
-      ]);
-    }
+    return Container(
+      margin: const EdgeInsets.only(left: 12, right: 12, top: 12),
+      child: Obx(() {
+        if (toggleController.isSelected[1]) {
+          // Ventilation
+          return Row(children: [
+            Flexible(flex: 4, child: getGraphView()),
+            Flexible(flex: 2, child: ventilationToggleView())
+          ]);
+        } else if (toggleController.isSelected[2]) {
+          // Defibrillation
+          return Row(children: [
+            Flexible(flex: 4, child: getGraphView()),
+            Flexible(flex: 2, child: defibrillationToggleView())
+          ]);
+        } else {
+          // Monitoring
+          return Row(children: [
+            Flexible(flex: 4, child: getGraphView()),
+            Flexible(flex: 2, child: monitoringToggleView())
+          ]);
+        }
+      }),
+    );
   }
 
   Widget getGraphView() {
@@ -84,7 +80,124 @@ class Monitor extends StatelessWidget {
     );
   }
 
-  Widget getToggleView() {
+  Widget monitoringToggleView() {
+    List<Map<String, Object>> infoData = [
+      {"type": "pPeak", "value": 50.12, "unit": " mBar"},
+      {"type": "pPlat", "value": 4.58, "unit": " mBar"},
+      {"type": "pMean", "value": 16.58, "unit": " mBar"},
+      {"type": "MV", "value": 7.2, "unit": " l/min"}
+    ];
+
+    List<Map<String, String>> settingData = [
+      {"name": "Freq.", "rate": "/min"},
+      {"name": "Vt", "rate": "ml"},
+      {"name": "PEEP", "rate": "mBar"},
+    ];
+
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Row(
+            children: const [
+              ValueTile(
+                sensor: sensorEnum.nibd,
+              ),
+              ValueTile(
+                sensor: sensorEnum.pulse,
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Row(
+            children: const [
+              ValueTile(
+                sensor: sensorEnum.mve,
+              ),
+              ValueTile(
+                sensor: sensorEnum.breathFrequency,
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: InfoTile(
+                  data: infoData,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: SettingTile(
+                  data: settingData,
+                ),
+              ),
+            ],
+          ),
+        ),
+        ModeToggleButton(),
+      ],
+    );
+  }
+
+  Widget ventilationToggleView() {
+    List<Map<String, Object>> infoData = [
+      {"type": "pPeak", "value": 50.12, "unit": " mBar"},
+      {"type": "pPlat", "value": 4.58, "unit": " mBar"},
+      {"type": "pMean", "value": 16.58, "unit": " mBar"},
+      {"type": "MV", "value": 7.2, "unit": " l/min"}
+    ];
+
+    List<Map<String, String>> settingData = [
+      {"name": "Freq.", "rate": "/min"},
+      {"name": "Vt", "rate": "ml"},
+      {"name": "PEEP", "rate": "mBar"},
+    ];
+
+    return Column(
+      children: [
+        Flexible(
+          flex: 1,
+          child: Row(
+            children: const [
+              ValueTile(
+                sensor: sensorEnum.mve,
+              ),
+              ValueTile(
+                sensor: sensorEnum.breathFrequency,
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(child: InfoTile(data: infoData)),
+              const ValueTile(
+                sensor: sensorEnum.breathFrequency,
+              ),
+            ],
+          ),
+        ),
+        Flexible(
+          flex: 2,
+          child: SettingTile(
+            data: settingData,
+          ),
+        ),
+        ModeToggleButton(),
+      ],
+    );
+  }
+
+  Widget defibrillationToggleView() {
     List<Map<String, Object>> infoData = [
       {"type": "pPeak", "value": 50.12, "unit": " mBar"},
       {"type": "pPlat", "value": 4.58, "unit": " mBar"},
