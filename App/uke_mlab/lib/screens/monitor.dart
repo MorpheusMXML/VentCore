7e876f1ad34e03dff1b8810ae579f004814642b0
Mudassar Zahid <mudassar.ahmad.zahid@gmail.com>
2021-12-22 20:37:48 +0100

    [TASK] #83 #90 Wrap Graph widgets in Obx to update list when another graph is added, Go back to accessing patientType via Get.arguments

--- App/uke_mlab/lib/screens/monitor.dart
+++ App/uke_mlab/lib/screens/monitor.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uke_mlab/models/system_state.dart';
+import 'package:uke_mlab/providers/start_screen_controller.dart';
 
-import 'package:uke_mlab/providers/mockup.dart';
 import 'package:uke_mlab/providers/style_controller.dart';
 import 'package:uke_mlab/providers/toggle_controller.dart';
 
@@ -13,7 +13,6 @@ import 'package:uke_mlab/widgets/setting/setting_tile.dart';
 import 'package:uke_mlab/widgets/value_box/value_tile.dart';
 import 'package:uke_mlab/widgets/graph/graph_adder.dart';
 
-import 'package:uke_mlab/models/model.dart';
 import 'package:uke_mlab/models/enums.dart';
 import 'package:uke_mlab/models/model_manager.dart';
 
@@ -22,10 +21,11 @@ import 'package:uke_mlab/utilities/screen_controller.dart';
 class Monitor extends StatelessWidget {
   final ModelManager modelManager = Get.find();
   final ScreenController screenController = Get.find();
-  final monitorController = Get.find<MonitorController>();
   final styleController = Get.put(StyleController());
   final toggleController = Get.find<ToggleController>();
   final SystemState systemState = Get.find<SystemState>();
+  final StartScreenController startScreenController =
+      Get.find<StartScreenController>();
 
   Monitor({
     Key? key,
@@ -33,7 +33,12 @@ class Monitor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    monitorController.activateTimer();
+    // TODO: [BUGFIX] Child --> Continue --> Menu --> Demo Scenarios --> Exit
+    // --> Skip --> ERROR
+    Get.arguments != null
+        ? startScreenController.selectedString.value =
+            Get.arguments["patientType"]
+        : print("Can't access patientType because of refresh");
 
     return Container(
       margin: const EdgeInsets.only(left: 12, right: 12, top: 12),
@@ -67,11 +72,13 @@ class Monitor extends StatelessWidget {
       child: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: systemState.graphList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GraphContainer(sensor: systemState.graphList[index]);
-              },
+            child: Obx(
+              () => ListView.builder(
+                itemCount: systemState.graphList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return GraphContainer(sensor: systemState.graphList[index]);
+                },
+              ),
             ),
           ),
           const GraphAdder(),
@@ -159,68 +166,6 @@ class Monitor extends StatelessWidget {
   }
 
   Widget defibrillationToggleView() {
-    List<Map<String, Object>> infoData = [
-      {"type": "pPeak", "value": 50.12, "unit": " mBar"},
-      {"type": "pPlat", "value": 4.58, "unit": " mBar"},
-      {"type": "pMean", "value": 16.58, "unit": " mBar"},
-      {"type": "MV", "value": 7.2, "unit": " l/min"}
-    ];
-
-    List<Map<String, String>> settingData = [
-      {"name": "Freq.", "rate": "/min"},
-      {"name": "Vt", "rate": "ml"},
-      {"name": "PEEP", "rate": "mBar"},
-    ];
-
-    return Column(
-      children: [
-        Flexible(
-          flex: 1,
-          child: Row(
-            children: const [
-              ValueTile(
-                sensor: sensorEnum.nibd,
-              ),
-              ValueTile(
-                sensor: sensorEnum.pulse,
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-          flex: 1,
-          child: Row(
-            children: const [
-              ValueTile(
-                sensor: sensorEnum.mve,
-              ),
-              ValueTile(
-                sensor: sensorEnum.breathFrequency,
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-          flex: 2,
-          child: Row(
-            children: [
-              Flexible(
-                flex: 1,
-                child: InfoTile(
-                  data: infoData,
-                ),
-              ),
-              Flexible(
-                flex: 1,
-                child: SettingTile(
-                  data: settingData,
-                ),
-              ),
-            ],
-          ),
-        ),
-        ModeToggleButton(),
-      ],
-    );
+    return ModeToggleButton();
   }
 }
