8bb3bbcb69b25e513bca9ea44c16f7e02d79746a
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-09 13:34:52 +0100

    [TASK] #105 Build Auto/Manual Button widgets with onPressed functionality and style

---
+++ App/uke_mlab/lib/widgets/defibrillation_mode/defibrilattion_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uke_mlab/providers/defibrillation_controller.dart';
+
+class DefibrillationButton extends StatelessWidget {
+  final String name;
+  const DefibrillationButton.auto({
+    Key? key,
+    this.name = 'Auto',
+  }) : super(key: key);
+
+  const DefibrillationButton.manual({
+    Key? key,
+    this.name = 'Manual',
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final DefibrillationController defibrillationController =
+        Get.find<DefibrillationController>();
+
+    return Expanded(
+      child: Container(
+          margin: const EdgeInsets.all(8),
+          child: Obx(() {
+            ButtonStyle buttonStyle;
+            defibrillationController.selected.value != name
+                ? buttonStyle = ElevatedButton.styleFrom(
+                    primary: Theme.of(context).cardColor,
+                    onPrimary: Theme.of(context).dividerColor)
+                : buttonStyle = ButtonStyle(
+                    side: MaterialStateProperty.all(
+                        const BorderSide(color: Colors.green)),
+                    foregroundColor: MaterialStateProperty.all(Colors.green),
+                  );
+            return ElevatedButton(
+              style: buttonStyle,
+              child: Text(name),
+              onPressed: defibrillationController.selected.value != name
+                  ? () => defibrillationController.setSelected(name)
+                  : null,
+            );
+          })),
+    );
+  }
+}
