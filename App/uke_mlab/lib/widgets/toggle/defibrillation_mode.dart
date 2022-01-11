b8c0064a70fda8a6c811f57dd57ae99dad38fa0a
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-11 23:43:19 +0100

    [TASK] #105 Adjust layout for defibrillation screen

--- App/uke_mlab/lib/widgets/toggle/defibrillation_mode.dart
+++ App/uke_mlab/lib/widgets/toggle/defibrillation_mode.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/defibrillation_container.dart';
+import 'package:uke_mlab/widgets/defibrillation_mode/defibrillation_stats.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/impedance_container.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/load_button.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/metronome_container.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/shock_power.dart';
 import 'package:uke_mlab/widgets/defibrillation_mode/sync_container.dart';
+import 'package:uke_mlab/widgets/defibrillation_mode/system_diagnosis.dart';
 import 'package:uke_mlab/widgets/toggle/toggle_mode_button.dart';
 
 class DefibrillationMode extends StatelessWidget {
@@ -16,22 +18,36 @@ class DefibrillationMode extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const DefibrillationContainer(),
-        const ShockPower(),
         Flexible(
-          child: Row(
-            children: [
-              Flexible(
-                child: Column(children: const [
-                  MetronomeContainer(),
-                  ImpedanceContainer(),
-                  SyncContainer(),
-                ]),
-              ),
-              const LoadButton(),
-            ],
+          flex: 3,
+          child: Container(
+            color: Theme.of(context).focusColor,
+            child: Column(
+              children: [
+                const SystemDiagnosis(),
+                const DefibrillationContainer(),
+                const ShockPower(),
+                Flexible(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        child: Column(children: const [
+                          MetronomeContainer(),
+                          ImpedanceContainer(),
+                          SyncContainer(),
+                        ]),
+                      ),
+                      const LoadButton(),
+                    ],
+                  ),
+                ),
+                const DefibrillationStats(),
+              ],
+            ),
           ),
         ),
+        const Spacer(),
         ToggleModeButton(),
       ],
     );
