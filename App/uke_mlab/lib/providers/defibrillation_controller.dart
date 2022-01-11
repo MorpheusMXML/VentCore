cfbd1f4b178be1961d293f7007f7b2f97580038e
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-11 23:41:57 +0100

    [TASK] #105 Add logic for Load widget and Stats widgets

--- App/uke_mlab/lib/providers/defibrillation_controller.dart
+++ App/uke_mlab/lib/providers/defibrillation_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 
 class DefibrillationController extends GetxController {
@@ -6,10 +8,24 @@ class DefibrillationController extends GetxController {
   RxString selectedSynchronicityButton = 'Sync'.obs;
 
   RxBool metronomeOn = false.obs;
-  RxBool loaded = true.obs;
+  RxString loaded = 'Loaded'.obs;
 
-  void toggleLoaded() {
-    loaded.toggle();
+  RxInt shockPower = 200.obs;
+  RxString systemDiagnosis = 'Placeholder Diagnosis'.obs;
+
+  RxString lastShock = ''.obs;
+  RxInt numberOfShocks = 0.obs;
+  Rx<Stopwatch> timer = Stopwatch().obs;
+
+  void toggleLoaded(String value) async {
+    if (loaded.value == 'Shock') {
+      numberOfShocks++;
+    } else {
+      loaded.value = 'Loading';
+      await Future.delayed(const Duration(seconds: 2));
+    }
+
+    loaded.value = value;
   }
 
   void toggleMetronome(bool changed) {
