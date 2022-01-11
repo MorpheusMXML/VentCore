d1502997e86374c5a846de0d646e14559694d1cc
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-11 23:42:56 +0100

    [TASK] #105 Create Stats widget

---
+++ App/uke_mlab/lib/widgets/defibrillation_mode/defibrillation_stats.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uke_mlab/providers/defibrillation_controller.dart';
+
+class DefibrillationStats extends StatelessWidget {
+  const DefibrillationStats({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DefibrillationController defibrillationController =
+        Get.find<DefibrillationController>();
+    defibrillationController.timer.value.start();
+
+    return Flexible(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Text('last'),
+              Text('# Shocks'),
+              Text('Timer'),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Obx(() => Text('${defibrillationController.lastShock}')),
+              Obx(() => Text('${defibrillationController.numberOfShocks}')),
+              Obx(
+                () => Text(
+                    '${defibrillationController.timer.value.elapsed.inSeconds}'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
