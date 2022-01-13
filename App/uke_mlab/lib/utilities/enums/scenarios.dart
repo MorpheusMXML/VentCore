73db50f613ea046724c254c518e81d5659b8fbbe
Noah Scheld <nickels12er@gmail.com>
2022-01-13 14:50:18 +0100

    [TASK] #108 added extension to enums -> scenarioPath

--- App/uke_mlab/lib/utilities/enums/scenarios.dart
+++ App/uke_mlab/lib/utilities/enums/scenarios.dart
@@ -7,3 +7,17 @@ enum scenariosEnum {
   scenario3c,
   scenario4
 }
+
+extension ScenarioEnumAttributes on scenariosEnum {
+  static const Map<scenariosEnum, String> attributes = {
+    scenariosEnum.standardScenario: 'assets/jsons/standard_scenario',
+    scenariosEnum.scenario1: 'assets/jsons/scenario1_data',
+    scenariosEnum.scenario2: 'assets/jsons/scenario2_data',
+    scenariosEnum.scenario3a: 'assets/jsons/scenario3_a_data',
+    scenariosEnum.scenario3b: 'assets/jsons/scenario3_b_data',
+    scenariosEnum.scenario3c: 'assets/jsons/scenario3_c_data',
+    scenariosEnum.scenario4: 'assets/jsons/scenario4_data'
+  };
+
+  String get scenarioPath => attributes[this]!;
+}
