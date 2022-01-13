9877118b3bf6a92cce9d565a89bf3c313105512f
Noah Scheld <nickels12er@gmail.com>
2022-01-13 14:49:13 +0100

    [REFACTOR] #108 rewrote to allow for split in sensorEnum

--- App/uke_mlab/lib/scenarios/standard_scenario.dart
+++ App/uke_mlab/lib/scenarios/standard_scenario.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
-import 'dart:convert';
 
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+
+import 'package:uke_mlab/models/model_absolute.dart';
+import 'package:uke_mlab/models/model_graph.dart';
 import 'package:uke_mlab/utilities/enums/sensor.dart';
 import 'package:uke_mlab/utilities/enums/scenarios.dart';
-import 'package:uke_mlab/models/model.dart';
 import 'package:uke_mlab/scenarios/abstract_scenario.dart';
 
 class StandardScenario extends AbstractScenario {
@@ -13,44 +13,70 @@ class StandardScenario extends AbstractScenario {
     scenarioType = scenariosEnum.standardScenario,
   }) : super();
 
-  //TODO: put into abstract class if we get values => use path as argument
   @override
-  Future<Map<sensorEnum, Map<String, dynamic>>> loadData() async {
-    Map<sensorEnum, Map<String, dynamic>> returnMap = {};
-    var jsonString = await rootBundle.loadString('assets/jsons/data.json');
-    var source = await jsonDecode(jsonString.toString())["data"];
-    returnMap = {
-      //TODO: numbers dont match sensors at the moment
-      sensorEnum.breathFrequency: source[0]["data"],
-      sensorEnum.co2: source[1]["data"],
-      sensorEnum.ecg: source[8]["data"],
-      sensorEnum.mve: source[3]["data"],
-      sensorEnum.nibd: source[4]["data"],
-      sensorEnum.pulse: source[5]["data"],
-      sensorEnum.spo2: source[7]["data"]
-    };
-    return returnMap;
-  }
+  void runScenario(
+      {required Map<sensorEnumAbsolute, Map<String, dynamic>> dataMapAbsolute,
+      required Map<sensorEnumGraph, Map<String, dynamic>> dataMapGraph}) {
+    for (var sensorAbsolute in dataMapAbsolute.keys) {
+      DataModelAbsolute dataModelAbsolute =
+          Get.find<DataModelAbsolute>(tag: sensorAbsolute.name);
+      double resolution =
+          dataMapAbsolute[sensorAbsolute]!['channel_information']['resolution']
+                  ['value']
+              .toDouble();
+      List<double> dataList = dataMapAbsolute[sensorAbsolute]!['data'];
 
-  @override
-  void runScenario(Map<sensorEnum, Map<String, dynamic>> dataMap) {
-    for (var sensor in sensorEnum.values) {
-      DataModel dataModel = Get.find<DataModel>(tag: sensor.name);
+      Timer.periodic(calculateUpdateRateAbsolute(resolution: resolution),
+          (timer) {
+        // TODO: intermediate implementation -> fix later
+        if (dataList.length - 1 == dataModelAbsolute.counter.value) {
+          dataModelAbsolute.counter.value = 0;
+        }
+        if (!scenarioRunning) {
+          timer.cancel();
+        }
 
-      Timer.periodic(Duration(milliseconds: sensor.updateRate * 100), (timer) {
-        var dataList = dataMap[sensor]!['data'];
-        var dataListLength = dataList.length - 1;
+        dataModelAbsolute
+            .updateValue(dataList[dataModelAbsolute.counter.value]);
+      });
+    }
 
-        var start = dataModel.singleData.value.counter % dataListLength;
-        var end = ((start + 100) % dataListLength) > start
-            ? (start + 100)
-            : dataListLength;
+    for (var sensorGraph in dataMapGraph.keys) {
+      DataModelGraph dataModelGraph =
+          Get.find<DataModelGraph>(tag: sensorGraph.name);
+      int batchSize = 1;
+      double resolution = dataMapGraph[sensorGraph]!['channel_information']
+              ['resolution']['value']
+          .toDouble();
+      List<double> dataList = dataMapGraph[sensorGraph]!['data'];
+
+      Timer.periodic(
+          calculateUpdateRate(batchSize: batchSize, resolution: resolution),
+          (timer) {
+        int startIndex = dataModelGraph.singleData.value.counter;
+        int endIndex = dataModelGraph.singleData.value.counter + batchSize;
 
         if (!scenarioRunning) {
           timer.cancel();
         }
 
-        dataModel.updateValueList(dataList.sublist(start, end));
+        // careful with sublist start inclusive end exclusive
+        // case: start is inside list.length but end overflows
+        if (endIndex > dataList.length - 1 &&
+            startIndex < dataList.length - 1) {
+          List endOfList = dataList.sublist(startIndex, dataList.length);
+          // may discard last value
+          List startOfList =
+              dataList.sublist(0, endIndex + 1 % dataList.length - 1);
+
+          dataModelGraph.updateValues(endOfList + startOfList);
+        } else if (startIndex > dataList.length - 1) {
+          dataModelGraph.updateValues(dataList.sublist(
+              startIndex % dataList.length - 1,
+              endIndex + 1 % dataList.length - 1));
+        } else {
+          dataModelGraph.updateValues(dataList.sublist(startIndex, endIndex));
+        }
       });
     }
   }
