c34519d0bc8154166e132937d46c11c39c33cdeb
Noah Scheld <nickels12er@gmail.com>
2022-01-15 18:16:39 +0100

    [TASK] #112 fixed errors when showing nibd graph

--- App/uke_mlab/lib/widgets/graph/history_graph.dart
+++ App/uke_mlab/lib/widgets/graph/history_graph.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:intl/intl.dart';
 import 'package:uke_mlab/models/model_graphdata.dart';
+import 'package:uke_mlab/models/model_nibd.dart';
+import 'package:uke_mlab/utilities/enums/sensor.dart';
 
 ///Creates a [SfCartesianChart] to Display a History Graph. Example Use: NIBD History
 ///
@@ -14,84 +17,95 @@ import 'package:uke_mlab/models/model_graphdata.dart';
 ///Uses [HiloSeries] to Plot the Systolic and Diastolic Pressure as Hi and Low of the Series.
 ///
 class HistoryGraph extends StatelessWidget {
-  ///Returns the Widget for a History Graph. Taking a [Map<String, Object?>] to plot the corresponding Data Points.
-  ///[Object?] can be of Type [NIBDdata] for example.
-  final Map<String, Object?> graphData;
+  final sensorEnumGraph sensor;
 
   const HistoryGraph({
     Key? key,
-    required this.graphData,
+    required this.sensor,
   }) : super(key: key);
 
   /// Efficient way to find min and Max of the Y-Axis Interval that should be Rendered in [HistoryGraph] given the data in [graphData].
-  double findMax(bool max) {
-    if (max) {
-      List<int> syst = [];
-      for (var element in (graphData['data'] as List<ChartData>)) {
-        syst.add(element.systolicPressure!);
-      }
-      syst.sort();
-
-      return syst.last.toDouble();
-    } else {
-      List<int> dist = [];
-
-      for (var element in (graphData['data'] as List<ChartData>)) {
-        dist.add(element.diastolicPressure!);
-      }
-
-      dist.sort();
-      return dist.first.toDouble();
+  double findMax(String type, DataModelNIBD dataModel) {
+    List dataList;
+    switch (type) {
+      case 'max':
+        dataList = (List.generate(dataModel.graphData.length,
+            (index) => dataModel.graphData[index].systolicPressure));
+        dataList.sort;
+        double max = dataList.last.toDouble();
+        print(max);
+        return dataList.isEmpty ? 150.0 : max;
+      case 'min':
+        dataList = (List.generate(dataModel.graphData.length,
+            (index) => dataModel.graphData[index].diastolicPressure));
+        dataList.sort;
+        double min = dataList.last.toDouble();
+        print(min);
+        return dataList.isEmpty ? 150.0 : min;
+      default:
+        return 0.0;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return SfCartesianChart(
-      tooltipBehavior: TooltipBehavior(enable: true),
-      backgroundColor: Theme.of(context).cardColor,
-      primaryYAxis: NumericAxis(
-        desiredIntervals: 10,
-        maximum: (findMax(true) + 5),
-        minimum: (findMax(false) - 5),
-        majorGridLines: MajorGridLines(
-          width: 1,
-          color: Theme.of(context).shadowColor,
-          dashArray: const <double>[2, 3],
+    DataModelNIBD dataModel = Get.find<DataModelNIBD>(tag: sensor.name);
+    return Obx(
+      () => SfCartesianChart(
+        tooltipBehavior: TooltipBehavior(enable: true),
+        backgroundColor: Theme.of(context).cardColor,
+        primaryYAxis: NumericAxis(
+          plotOffset: 5,
+          desiredIntervals: 10,
+          //maximum: 150.0, //findMax('max', dataModel) + 5,
+          //minimum: 0.0, //findMax('min', dataModel) - 5,
+          majorGridLines: MajorGridLines(
+            width: 1,
+            color: Theme.of(context).shadowColor,
+            dashArray: const <double>[2, 3],
+          ),
         ),
+        primaryXAxis: DateTimeAxis(
+          dateFormat: DateFormat.Hm(),
+          plotOffset: 10,
+          desiredIntervals: 25,
+          majorGridLines:
+              MajorGridLines(width: 0, color: Theme.of(context).shadowColor),
+        ),
+        series: [
+          // Renders scatter chart
+          ScatterSeries(
+            markerSettings: const MarkerSettings(
+                height: 10,
+                width: 10,
+                // Scatter will render in diamond shape
+                shape: DataMarkerType.diamond),
+            trendlines: <Trendline>[
+              Trendline(
+                  dashArray: <double>[2, 3],
+                  type: TrendlineType.movingAverage,
+                  color: Colors.redAccent)
+            ],
+            color: dataModel.color,
+            //ARNE Sagt: Löscht du das = Kopf Ab!!!!!!11!
+            dataSource: dataModel.graphData.value,
+            xValueMapper: (ChartData data, _) => data.time,
+            yValueMapper: (ChartData data, _) => data.meanArterialPressure,
+          ),
+          HiloSeries(
+            enableTooltip: true,
+            dataSource: dataModel.graphData.value,
+            color: Colors.red,
+            xValueMapper: (ChartData data, _) => data.time,
+            lowValueMapper: (ChartData data, _) => data.diastolicPressure,
+            highValueMapper: (ChartData data, _) => data.systolicPressure,
+            markerSettings: const MarkerSettings(
+                shape: DataMarkerType.horizontalLine,
+                width: 10,
+                isVisible: true),
+          ),
+        ],
       ),
-      primaryXAxis: DateTimeAxis(
-        dateFormat: DateFormat.Hm(),
-        plotOffset: 10,
-        desiredIntervals: 25,
-        majorGridLines: MajorGridLines(width: 0, color: Theme.of(context).shadowColor),
-      ),
-      series: [
-        // Renders scatter chart
-        ScatterSeries(
-          markerSettings: const MarkerSettings(
-              height: 10,
-              width: 10,
-              // Scatter will render in diamond shape
-              shape: DataMarkerType.diamond),
-          trendlines: <Trendline>[
-            Trendline(dashArray: <double>[2, 3], type: TrendlineType.movingAverage, color: Colors.redAccent)
-          ],
-          color: graphData['color'] as Color,
-          dataSource: graphData['data'] as List<ChartData>,
-          xValueMapper: (ChartData data, _) => data.time,
-          yValueMapper: (ChartData data, _) => data.meanArterialPressure,
-        ),
-        HiloSeries(
-          enableTooltip: true,
-          dataSource: graphData['data'] as List<ChartData>,
-          color: Colors.red,
-          xValueMapper: (ChartData data, _) => data.time,
-          lowValueMapper: (ChartData data, _) => data.diastolicPressure,
-          highValueMapper: (ChartData data, _) => data.systolicPressure,
-          markerSettings: const MarkerSettings(shape: DataMarkerType.horizontalLine, width: 10, isVisible: true),
-        ),
-      ],
     );
   }
 }
