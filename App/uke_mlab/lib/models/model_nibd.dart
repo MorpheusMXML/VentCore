6467d44a265f35bf2295386d20993a5e63ca7c1f
Noah Scheld <nickels12er@gmail.com>
2022-01-15 18:12:43 +0100

    [TASK] #112 wrote entire model for nibdData

--- App/uke_mlab/lib/models/model_nibd.dart
+++ App/uke_mlab/lib/models/model_nibd.dart
@@ -1,11 +1,52 @@
-class NIBDdata {
-  final DateTime time;
-  final int systolicPressure;
-  final int diastolicPressure;
-  late int mad;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:uke_mlab/models/model_graphdata.dart';
+import 'package:uke_mlab/utilities/enums/sensor.dart';
 
-  NIBDdata(this.time, this.systolicPressure, this.diastolicPressure) {
-    //Formular for Calculating MAD out of systolic and diastolic pressure
-    mad = (diastolicPressure + (1 / 3) * (systolicPressure - diastolicPressure)).toInt();
+class DataModelNIBD extends GetxController {
+  final sensorEnumGraph sensorKey;
+
+  String graphTitle = "No default title";
+  String yAxisTitle = "No default yAxisTitle";
+  String xAxisTitle = "No default xAxisTitle";
+  Color color = Colors.white;
+
+  late final Rx<ChartData> singleData;
+  final RxList<ChartData> graphData = <ChartData>[].obs;
+  int graphDataMaxLength = 1500;
+
+  final RxList<List<int>> historicValues = <List<int>>[].obs;
+
+  DataModelNIBD({required this.sensorKey}) {
+    singleData =
+        ChartData.asNIBD(counter: 0, time: DateTime.now(), value: <int>[0, 0])
+            .obs;
+  }
+
+  void updateValues(List<dynamic> valueList) {
+    for (int i = 0; i < valueList.length; i++) {
+      singleData.value = ChartData.asNIBD(
+          time: DateTime.now(),
+          value: valueList[i],
+          counter: singleData.value.counter + 1);
+      graphData.add(singleData.value);
+    }
+
+    if (graphData.length + 1 > graphDataMaxLength) {
+      for (var i = 0; i < valueList.length; i++) {
+        graphData.removeAt(0);
+      }
+    }
+    // update only added/removed indexes instead of the whole chart (efficient)
+    // chartController?.updateDataSource(updatedDataIndexes: [for (int i = 0; i < graphData.length; i++) i]);
+
+    // TODO make "little" analysis here and call inform alarmManager to start evaluation
+  }
+
+  void resetDataModel() {
+    singleData.value =
+        ChartData.asNIBD(time: DateTime.now(), value: <int>[0, 0], counter: 0);
+    //clear historical data
+    graphData.clear();
   }
 }
