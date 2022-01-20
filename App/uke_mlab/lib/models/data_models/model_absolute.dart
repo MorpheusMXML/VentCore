da9d0d0a4e90ca4366f93db51cc61b59b47b6633
Arnatar <arnestruck@astruck.de>
2022-01-20 15:34:48 +0100

    [TASK] #103 comments for model manager

--- App/uke_mlab/lib/models/data_models/model_absolute.dart
+++ App/uke_mlab/lib/models/data_models/model_absolute.dart
@@ -9,7 +9,7 @@ import 'package:uke_mlab/utilities/enums/sensor.dart';
 import 'package:uke_mlab/widgets/value_box/value_box_container.dart';
 
 class DataModelAbsolute extends GetxController {
-  /// represents the data model to be used in [ValueBoxContainer]
+  /// represents the data model to be used in [ValueBoxContainer] and subwidgets
 
   /// sensor key hinting at the corresponding sensor addressing the current [DataModelAbsolute]
   late final sensorEnumAbsolute sensorKey;
