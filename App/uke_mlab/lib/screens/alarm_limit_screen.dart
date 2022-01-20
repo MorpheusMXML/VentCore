997ea005f891f950c2f3c0d87426be8341773092
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-20 19:11:43 +0100

    [TASK] #91 Create Gridbuilder for Alarm Limits Screen

--- App/uke_mlab/lib/screens/alarm_limit_screen.dart
+++ App/uke_mlab/lib/screens/alarm_limit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:uke_mlab/utilities/enums/sensor.dart';
 import 'package:uke_mlab/widgets/demo_screen/exit_button.dart';
 import 'package:uke_mlab/widgets/value_box/value_box_tile.dart';
@@ -10,21 +11,60 @@ class AlarmLimitScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        SizedBox(
-          height: 150,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: sensorEnumAbsolute.values
-                .map((sensor) => ValueBoxTile(sensorAbsolute: sensor))
-                .toList(),
+        Flexible(child: Container()),
+        Flexible(
+          flex: 3,
+          child: Column(
+            children: [
+              const Text('Monitoring', style: TextStyle(fontSize: 32)),
+              GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: [
+                    sensorEnumAbsolute.hfAbsolute,
+                    sensorEnumAbsolute.spo2Absolute,
+                    sensorEnumAbsolute.tempAbsolute,
+                    sensorEnumAbsolute.sysAbsolute,
+                    sensorEnumAbsolute.co2Absolute,
+                    sensorEnumAbsolute.diaAbsolute,
+                    sensorEnumAbsolute.pulse,
+                  ]
+                      .map((sensor) =>
+                          ValueBoxTile.withHeadline(sensorAbsolute: sensor))
+                      .toList()),
+            ],
           ),
         ),
-        const Spacer(),
-        // TODO: maybe new file?
-        const ExitButton()
+        Flexible(child: Container()),
+        Flexible(
+          flex: 3,
+          child: Column(
+            children: [
+              const Text('Ventilation', style: TextStyle(fontSize: 32)),
+              GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  children: [
+                    sensorEnumAbsolute.hfAbsolute,
+                    sensorEnumAbsolute.mve,
+                    sensorEnumAbsolute.breathfrequency,
+                  ]
+                      .map((sensor) =>
+                          ValueBoxTile.withHeadline(sensorAbsolute: sensor))
+                      .toList()),
+            ],
+          ),
+        ),
+        Flexible(flex: 4, child: Container())
       ],
     );
   }
 }
+
+/*
+  const Spacer(),
+  // TODO: maybe new file?
+  const ExitButton()
+*/
