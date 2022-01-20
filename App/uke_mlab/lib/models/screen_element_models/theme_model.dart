1d100604b6a79373a2760855d3fab5d4fea68d64
Arnatar <arnestruck@astruck.de>
2022-01-20 14:43:49 +0100

    [TASK] #103 commentary for theme model added

--- App/uke_mlab/lib/models/screen_element_models/theme_model.dart
+++ App/uke_mlab/lib/models/screen_element_models/theme_model.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uke_mlab/utilities/app_theme.dart';
+import 'package:uke_mlab/widgets/menu/toggle_theme_button.dart';
 
 class ThemeModel {
+  /// contains information about the current active [AppTheme]
+
+  /// flag whether dark mode is currently active, starts at true
   RxBool isDarkMode = true.obs;
 
+  /// the icon representing the current [AppTheme] on the [ToggleThemeButton]
   Rx<Icon> toggleIcon = const Icon(Icons.dark_mode).obs;
 
+  /// toggles the current [AppTheme] from light to dark mode
   void toggleTheme() {
     if (isDarkMode.value) {
       Get.changeTheme(AppTheme.lightTheme);
