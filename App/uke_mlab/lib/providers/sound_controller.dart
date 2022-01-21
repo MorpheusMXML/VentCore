087c985574eed693730421fd21632a3e75a398c1
Arnatar <arnestruck@astruck.de>
2022-01-21 14:33:02 +0100

    [Fix] #125 fixed some of the double timers

--- App/uke_mlab/lib/providers/sound_controller.dart
+++ App/uke_mlab/lib/providers/sound_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:get/get.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:uke_mlab/models/data_models/model_absolute.dart';
@@ -8,11 +9,11 @@ import 'package:uke_mlab/utilities/enums/sensor.dart';
 /// Alarms can be Triggered, stopped and the SpO2 / ECG Sound for the Heartfrequency can be started.
 ///
 /// ### Class Variables
-/// - [alarmPlayer] loads a Instance of Type [AudioCache] and sets the sounds Folder prefix.
-/// + [ecgPlayer] loads a Instance of Type [AudioCache] and sets the sounds Folder prefix.
+/// - [alarmPlayerCache] loads a Instance of Type [AudioCache] and sets the sounds Folder prefix.
+/// + [ecgPlayerCache] loads a Instance of Type [AudioCache] and sets the sounds Folder prefix.
 /// + [timer] holds a Inscante of Type [Timer] to control the ECG Sunds Timing
 /// + [alarmSoundFiles] & [ecgSoundFiles] provide a Map where the Sounds for the corresponding Alarm or ECG Sound can be specified.
-/// + [alarmPlayerRet] & [ecgPlayerRet] hold a late [AudioPlayer] Instance to controll the Audio after Triggering it.
+/// + [alarmPlayer] & [ecgPlayer] hold a late [AudioPlayer] Instance to controll the Audio after Triggering it.
 ///
 /// ### Methods
 /// + [play(Enum SoundIdentifier)] starts the alarm SoundIdentifier.
@@ -21,10 +22,15 @@ import 'package:uke_mlab/utilities/enums/sensor.dart';
 ///
 class SoundController {
   ///Loads AudioAlarm Files into Cache and Provides functionality to Play, Stop them. Also provides ECG Sound per Minute and O2 Saturation.
-  final AudioCache alarmPlayer = AudioCache(prefix: 'assets/sounds/');
-  final AudioCache ecgPlayer = AudioCache(prefix: 'assets/sounds/');
+  final AudioCache alarmPlayerCache = AudioCache(prefix: 'assets/sounds/');
+  final AudioCache ecgPlayerCache = AudioCache(prefix: 'assets/sounds/');
   Timer? timer;
   Timer? getDataTimer;
+  Timer? cancelTimerBeep;
+  final int getDataTimerDuration = 5;
+
+  AudioPlayer? alarmPlayer;
+  AudioPlayer? ecgPlayer;
 
   /// This [Map<SoundIdentifier, String>] specifies the AlarmType in the [Enum] Key and maps it to the [String] of the Sound Files Name in he Assets/sounds
   /// Folder. If you wish to change the Alarm Sound Files that shall pe blayed. Change them accordingly here and place the Sondfile under [./App/uke_mlab/assets/sounds]
@@ -44,30 +50,29 @@ class SoundController {
     SoundIdentifier.hF50: 'ECG_50wav.wav',
   };
 
-  AudioPlayer? alarmPlayerRet;
-  AudioPlayer? ecgPlayerRet;
-
   ///Constructor for this. Loads the specified Sound files from [alarmSoundFiles] & [ecgecgSoundFiles] into the Cache of the Application.
   SoundController() {
-    alarmPlayer.loadAll(alarmSoundFiles.values.toList());
-    ecgPlayer.loadAll(ecgSoundFiles.values.toList());
+    alarmPlayerCache.loadAll(alarmSoundFiles.values.toList());
+    ecgPlayerCache.loadAll(ecgSoundFiles.values.toList());
   }
 
   ///playes the SoundAlarm fot the AlarmType specified with [Enum soundIdentifier].
   play(Enum soundIdentifier) async {
-    alarmPlayerRet =
-        await alarmPlayer.play(alarmSoundFiles[soundIdentifier].toString());
+    alarmPlayer = await alarmPlayerCache
+        .play(alarmSoundFiles[soundIdentifier].toString());
   }
 
   ///sops all [AudioPlayer]'s that are currently playing a Sound.
   stop() async {
-    if (alarmPlayerRet != null) {
-      alarmPlayerRet!.stop();
-      alarmPlayerRet = null;
+    if (alarmPlayer != null) {
+      alarmPlayer!.stop();
+      alarmPlayer!.dispose();
+      alarmPlayer = null;
     }
-    if (ecgPlayerRet != null) {
-      ecgPlayerRet!.stop();
-      ecgPlayerRet = null;
+    if (ecgPlayer != null) {
+      ecgPlayer!.stop();
+      ecgPlayer!.dispose();
+      ecgPlayer = null;
     }
     if (timer != null) {
       timer!.cancel();
@@ -85,8 +90,8 @@ class SoundController {
   /// The Rhythm the ECG Sound playes is calculated by getting the Hz Freq. of the Sound and transfoming it into the Milliseconds they have to be distributed at.
   /// Creates a [Duration] and stores the duration needed till the next Sound hast to be played.
   ///
-  /// Creates a [Timer.periodic()] to repeat the play Function of the [ecgPlayer]. This triggeres the Sound chosen depending on the [spO2] this method got called with.
-  /// Stores the AudioPlayer controller in [ecgPlayerRet] to keep access on stopping the Sound with [this.stop()].
+  /// Creates a [Timer.periodic()] to repeat the play Function of the [ecgPlayerCache]. This triggeres the Sound chosen depending on the [spO2] this method got called with.
+  /// Stores the AudioPlayer controller in [ecgPlayer] to keep access on stopping the Sound with [this.stop()].
   ///
   /// ### Rhythm Calculation and Timing
   ///
@@ -96,8 +101,9 @@ class SoundController {
   ///Duration duration = Duration(milliseconds: milliesTillNext);
   ///```
   saturationHfBeep({required int bpm, required int spO2}) async {
+    const double volume = 0.2;
     String ecgSound = ecgSoundFiles[SoundIdentifier.hFnormal].toString();
-    ecgPlayerRet = await ecgPlayer.play(ecgSound, volume: 0);
+    //ecgPlayerRet = await ecgPlayer.play(ecgSound, volume: 0);
 
     if (spO2 > 90) {
       ecgSound = ecgSoundFiles[SoundIdentifier.hFnormal].toString();
@@ -111,6 +117,11 @@ class SoundController {
       ecgSound = ecgSoundFiles[SoundIdentifier.hF50].toString();
     }
 
+    if (timer != null) {
+      timer?.cancel();
+      timer = null;
+    }
+
     if (bpm != 0) {
       double beepHz = bpm / 60;
       int milliesTillNext = ((1 / beepHz) * 1000).toInt();
@@ -120,16 +131,67 @@ class SoundController {
         print('stopping Player');
         ecgPlayerRet.stop();
       } */
-      if (timer != null) {
-        timer?.cancel();
-      }
 
-      timer = Timer.periodic(duration, ((timer) async {
-        ecgPlayerRet = await ecgPlayer.play(ecgSound, volume: 0.7);
-      }));
+      timer = Timer.periodic(
+        duration,
+        ((timer) async {
+          if (Platform.isAndroid) {
+            ecgPlayer = await ecgPlayerCache.play(
+              ecgSound,
+              volume: volume,
+              mode: PlayerMode.LOW_LATENCY,
+            );
+            // since stayAwake is not implemented on macOs, we like to check
+          } else if (Platform.isIOS) {
+            ecgPlayer = await ecgPlayerCache.play(
+              ecgSound,
+              volume: volume,
+              mode: PlayerMode.LOW_LATENCY,
+            );
+          }
+
+          // fix for double timer after hot reload, cancels running timer 1 second after a reload of method should have occoured
+          if (cancelTimerBeep != null) {
+            cancelBeepTimer(duration);
+          }
+        }),
+      );
     } else {
-      ecgPlayerRet = await ecgPlayer.loop(ecgSound, volume: 0.7);
+      timer = Timer.periodic(
+        Duration(seconds: getDataTimerDuration),
+        ((timer) async {
+          if (Platform.isAndroid) {
+            ecgPlayer = await ecgPlayerCache.play(
+              ecgSound,
+              volume: volume,
+              mode: PlayerMode.LOW_LATENCY,
+            );
+            // since stayAwake is not implemented on macOs, we like to check
+          } else if (Platform.isIOS) {
+            ecgPlayer = await ecgPlayerCache.play(
+              ecgSound,
+              volume: volume,
+              mode: PlayerMode.LOW_LATENCY,
+            );
+          }
+
+          // fix for double timer after hot reload, cancels running timer 1 second after a reload of method should have occoured
+          if (cancelTimerBeep != null) {
+            cancelBeepTimer(Duration(seconds: getDataTimerDuration + 1));
+          }
+        }),
+      );
     }
+  }
+
+  // internal method, to fix double timers after hot reload
+  void cancelBeepTimer(Duration duration) {
+    cancelTimerBeep = Timer(duration, () {
+      timer?.cancel();
+      timer = null;
+      cancelTimerBeep?.cancel();
+      cancelTimerBeep = null;
+    });
   }
 
   void startSaturationHFSound() {
@@ -137,7 +199,8 @@ class SoundController {
         Get.find<DataModelAbsolute>(tag: sensorEnumAbsolute.hfAbsolute.name);
     DataModelAbsolute spo2Model =
         Get.find<DataModelAbsolute>(tag: sensorEnumAbsolute.spo2Absolute.name);
-    getDataTimer ??= Timer.periodic(const Duration(seconds: 5), (timer) {
+    getDataTimer ??=
+        Timer.periodic(Duration(seconds: getDataTimerDuration), (timer) {
       saturationHfBeep(
           bpm: hfModel.absoluteValue.value.toInt(),
           spO2: spo2Model.absoluteValue.value.toInt());
