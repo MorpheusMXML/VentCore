0d8da40a1c97f0b9208487f6afcafc5a0f985d64
Morpheus <mail@maxbrosius.de>
2022-01-21 14:33:13 +0100

    [FIX] Fixed Minor Bug where 2 players played at same time, changed Animation Duration to fit Sound Behaviour

--- App/uke_mlab/lib/providers/sound_controller.dart
+++ App/uke_mlab/lib/providers/sound_controller.dart
@@ -64,7 +64,7 @@ class SoundController {
   }
 
   playDefiLoadSound() async {
-    ecgPlayerRet!.stop();
+    stop();
     alarmPlayerRet = await alarmPlayer.play(_alarmSoundFiles[SoundIdentifier.defiLoading].toString());
     alarmPlayerRet!.onPlayerCompletion.listen((event) async {
       alarmPlayerRet = await alarmPlayer.loop(_alarmSoundFiles[SoundIdentifier.defiShockReady].toString());
