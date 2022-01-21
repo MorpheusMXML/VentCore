4b7e94ca94a3cf0b4ded3760854b0c3949f497b8
Morpheus <mail@maxbrosius.de>
2022-01-21 23:40:54 +0100

    [Task] #143 added General Alarms with Timings from Patient Scenarios

--- App/uke_mlab/lib/utilities/enums/scenarios.dart
+++ App/uke_mlab/lib/utilities/enums/scenarios.dart
@@ -1,15 +1,8 @@
+import 'package:uke_mlab/utilities/enums/alarm_status.dart';
 import 'package:uke_mlab/utilities/enums/non_graph_alarm.dart';
 import 'package:uke_mlab/utilities/enums/sensor.dart';
 
-enum scenariosEnum {
-  standardScenario,
-  scenario1,
-  scenario2,
-  scenario3a,
-  scenario3b,
-  scenario3c,
-  scenario4
-}
+enum scenariosEnum { standardScenario, scenario1, scenario2, scenario3a, scenario3b, scenario3c, scenario4 }
 
 extension ScenarioEnumPath on scenariosEnum {
   static const Map<scenariosEnum, String> attributes = {
@@ -26,54 +19,111 @@ extension ScenarioEnumPath on scenariosEnum {
 }
 
 extension ScenarioEnumAttributes on scenariosEnum {
-  static const Map<scenariosEnum,
-          Map<sensorEnumGraph, Map<int, Map<String, dynamic>>>>
-      nonMeasurableAlarms = {
+  static const Map<scenariosEnum, Map<sensorEnumGraph, Map<int, Map<String, dynamic>>>> nonMeasurableAlarms = {
     scenariosEnum.standardScenario: {
-      sensorEnumGraph.paw: {},
-      sensorEnumGraph.flow: {},
-      sensorEnumGraph.co2: {},
       sensorEnumGraph.pleth: {
         700: {
           'alarm': nonGraphAlarmEnum.o2empty,
           'priority': 20,
         },
       },
-      sensorEnumGraph.ecgCh1: {},
-      sensorEnumGraph.ecgCh2: {},
-      sensorEnumGraph.nibd: {},
     },
     scenariosEnum.scenario1: {
       sensorEnumGraph.pleth: {
-        800: {
-          'alarm': nonGraphAlarmEnum.o2empty,
-          'priority': 20,
-        },
-        1000: {
-          'alarm': nonGraphAlarmEnum.o2low,
-          'priority': 60,
-        },
-        1200: {
-          'alarm': nonGraphAlarmEnum.cprFeedback,
-          'priority': 100,
+        300: {
+          'alarm': nonGraphAlarmEnum.spo2bad,
+          'priority': 50 //alarmStatus.warning,
         },
       },
-      sensorEnumGraph.ecgCh1: {},
-      sensorEnumGraph.ecgCh2: {},
-      sensorEnumGraph.nibd: {},
     },
     scenariosEnum.scenario2: {
-      sensorEnumGraph.co2: {},
-      sensorEnumGraph.pleth: {},
-      sensorEnumGraph.ecgCh1: {},
-      sensorEnumGraph.ecgCh2: {},
-      sensorEnumGraph.nibd: {},
+      sensorEnumGraph.ecgCh1: {
+        2500: {
+          'alarm': nonGraphAlarmEnum.vt,
+          'priority': 50 //alarmStatus.middle,
+        },
+        3500: {
+          'alarm': nonGraphAlarmEnum.leckageHigh,
+          'priority': 50 //alarmStatus.middle,
+        },
+        4000: {
+          'alarm': nonGraphAlarmEnum.ventDisconnect,
+          'priority': 50 //alarmStatus.middle,
+        },
+        10000: {
+          'alarm': nonGraphAlarmEnum.o2low,
+          'priority': 50 // alarmStatus.middle,
+        },
+        15000: {
+          'alarm': nonGraphAlarmEnum.o2empty,
+          'priority': 50 // alarmStatus.high,
+        },
+        18500: {
+          'alarm': nonGraphAlarmEnum.ekgArtifact,
+          'priority': 50 //alarmStatus.warning,
+        },
+        100000: {
+          'alarm': nonGraphAlarmEnum.lostConnection,
+          'priority': 50 //alarmStatus.high,
+        }
+      },
     },
-    scenariosEnum.scenario3a: {},
-    scenariosEnum.scenario3b: {},
-    scenariosEnum.scenario3c: {},
-    scenariosEnum.scenario4: {},
+    scenariosEnum.scenario3a: {
+      sensorEnumGraph.ecgCh2: {
+        2000: {
+          'alarm': nonGraphAlarmEnum.ventDisconnect,
+          'priority': 50 // alarmStatus.high,
+        },
+        4000: {
+          'alarm': nonGraphAlarmEnum.ekgNotConnected,
+          'priority': 50 // alarmStatus.high,
+        },
+        5500: {
+          'alarm': nonGraphAlarmEnum.spo2disconnected,
+          'priority': 50 // alarmStatus.middle,
+        },
+        60000: {
+          'alarm': nonGraphAlarmEnum.vf,
+          'priority': 50 // alarmStatus.high,
+        }
+      }
+    },
+    scenariosEnum.scenario3b: {
+      sensorEnumGraph.pleth: {
+        800: {
+          'alarm': nonGraphAlarmEnum.spo2bad,
+          'priority': 50 // alarmStatus.warning,
+        },
+      }
+    },
+    scenariosEnum.scenario3c: {
+      sensorEnumGraph.ecgCh2: {
+        2000: {
+          'alarm': nonGraphAlarmEnum.ekgArtifact,
+          'priority': 50 // alarmStatus.warning,
+        },
+        15500: {
+          'alarm': nonGraphAlarmEnum.spo2bad,
+          'priority': 50 // alarmStatus.warning,
+        },
+        9500: {
+          'alarm': nonGraphAlarmEnum.vt,
+          'priority': 50 // alarmStatus.warning,
+        },
+      }
+    },
+    scenariosEnum.scenario4: {
+      sensorEnumGraph.ecgCh2: {
+        2000: {
+          'alarm': nonGraphAlarmEnum.vt,
+          'priority': 50 // alarmStatus.high,
+        },
+        4500: {
+          'alarm': nonGraphAlarmEnum.spo2bad,
+          'priority': 50 // alarmStatus.middle,
+        },
+      }
+    },
   };
-  Map<sensorEnumGraph, Map<int, Map<String, dynamic>>> get scenarioMap =>
-      nonMeasurableAlarms[this]!;
+  Map<sensorEnumGraph, Map<int, Map<String, dynamic>>> get scenarioMap => nonMeasurableAlarms[this]!;
 }
