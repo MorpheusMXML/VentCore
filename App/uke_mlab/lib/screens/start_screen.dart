8da0b7b44fc46ae664b39f12bded5867b2d44b8c
Morpheus <mail@maxbrosius.de>
2022-01-22 14:30:48 +0100

    [HOTFIX] #147 Removed unnecessary Comments, Imports

--- App/uke_mlab/lib/screens/start_screen.dart
+++ App/uke_mlab/lib/screens/start_screen.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart';
-import 'dart:async';
-import 'dart:math';
-import 'package:uke_mlab/models/screen_element_models/general_alarms.dart';
-import 'package:uke_mlab/models/system_state.dart';
-import 'package:uke_mlab/utilities/enums/non_graph_alarm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 import 'package:uke_mlab/widgets/start_screen/aed_button.dart';
 import 'package:uke_mlab/widgets/start_screen/patient_type_button.dart';
 import 'package:uke_mlab/widgets/start_screen/continue_button.dart';
@@ -20,23 +15,8 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // For general alarm testing purposes, TODO: Remove
-    //final GeneralAlarms generalAlarms = Get.find<SystemState>().generalAlarms;
-    //Timer.periodic(const Duration(seconds: 2), (timer) {
-    //  if (Random().nextInt(10) > 1) {
-    //    generalAlarms.addAlarm(
-    //        nonGraphAlarmEnum
-    //            .values[Random().nextInt(nonGraphAlarmEnum.values.length)],
-    //        Random().nextInt(100));
-    //  } else {
-    //    if (generalAlarms.alarmList.isNotEmpty) {
-    //      generalAlarms.removeAlarm(generalAlarms.alarmList[0].alarm);
-    //    }
-    //  }
-    //});
-
     Map<String, String> imageMap = {
-      'Adult': 'assets/icons/adults2.svg',
+      'Adult': 'assets/icons/parents.svg',
       'Child': 'assets/icons/children.svg',
       'Infant': 'assets/icons/newborn.svg',
     };
@@ -48,6 +28,7 @@ class StartScreen extends StatelessWidget {
         child: Row(
           children: [
             Flexible(
+              flex: 2,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children:
@@ -56,12 +37,17 @@ class StartScreen extends StatelessWidget {
                     imageMap.entries
                         .map((entry) => PatientTypeButton(
                               name: entry.key,
-                              image: SvgPicture.asset(entry.value, height: 70),
+                              image: SvgPicture.asset(
+                                entry.value,
+                                height: 70,
+                                width: 70,
+                              ),
                             ))
                         .toList(),
               ),
             ),
             Flexible(
+              flex: 3,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
