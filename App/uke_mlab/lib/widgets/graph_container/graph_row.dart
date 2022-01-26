0761471de3ebadd3ab7334c8126eb4950a2cdc61
Mudassar Zahid <mudassar.zahid@studium.uni-hamburg.de>
2022-01-26 18:31:54 +0100

    [TASK] #103 Add comments to GraphRow

--- App/uke_mlab/lib/widgets/graph_container/graph_row.dart
+++ App/uke_mlab/lib/widgets/graph_container/graph_row.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 import 'package:uke_mlab/utilities/enums/sensor.dart';
+import 'package:uke_mlab/widgets/graph/co2_graph.dart';
+import 'package:uke_mlab/widgets/graph/cpr_graph.dart';
 
-import 'package:uke_mlab/widgets/graph/graph.dart';
+import 'package:uke_mlab/widgets/graph/regular_graph.dart';
 import 'package:uke_mlab/widgets/value_box/value_box_tile.dart';
 import 'package:uke_mlab/widgets/graph/history_graph.dart';
 
-/// renders the actual [Graph] for instance [HistoryGraph] and the [ValueBoxTile]
+/// Renders the row consisting of a graph ([CprGraph], [HistoryGraph], [CO2Graph] and [RegularGraph]) and a [ValueBoxTile] (optional).
+///
+/// Uses:
+/// + [CprGraph]
+/// + [HistoryGraph]
+/// + [CO2Graph]
+/// + [RegularGraph]
+/// + [ValueBoxTile]
 class GraphRow extends StatelessWidget {
   /// Key to identify the corresponding sensor
   final sensorEnumGraph sensor;
@@ -21,21 +30,39 @@ class GraphRow extends StatelessWidget {
       constraints: const BoxConstraints(maxHeight: 150),
       child: Row(children: [
         Expanded(
-          child: Graph(sensor: sensor),
+          child: evaluateGraph(),
         ),
-        if (sensor != sensorEnumGraph.nibd) ...[
-          const SizedBox(width: 8),
-          evaluateValueBoxTile()
-        ]
+        if (sensor != sensorEnumGraph.nibd)
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: evaluateValueBoxTile(),
+          )
       ]),
     );
   }
 
-  Widget evaluateValueBoxTile() {
-    if (SensorMapping.sensorMap[sensor] == null) {
-      return ValueBoxTile.withoutAbsolute(sensorGraph: sensor);
-    } else {
-      return ValueBoxTile(sensorAbsolute: SensorMapping.sensorMap[sensor]);
+  /// Evaluates what type of [ValueBoxTile] will be shown. The decision depends on the provided sensor.
+  ValueBoxTile evaluateValueBoxTile() {
+    return SensorMapping.sensorMap[sensor] == null
+        ? ValueBoxTile.withoutAbsolute(sensorGraph: sensor)
+        : ValueBoxTile(sensorAbsolute: SensorMapping.sensorMap[sensor]);
+  }
+
+  /// Evaluates what type of graph will be shown. The decision depends on the provided sensor.
+  /// [sensorEnumGraph.cpr] is should use the [CprGraph].
+  /// [sensorEnumGraph.nibd] is should use the [HistoryGraph].
+  /// [sensorEnumGraph.co2] is should use the [CO2Graph].
+  /// Every other instance of [sensorEnumGraph] should use the [RegularGraph].
+  Widget evaluateGraph() {
+    switch (sensor) {
+      case sensorEnumGraph.cpr:
+        return CprGraph(sensor: sensor);
+      case sensorEnumGraph.nibd:
+        return HistoryGraph(sensor: sensor);
+      case sensorEnumGraph.co2:
+        return CO2Graph(sensor: sensor);
+      default:
+        return RegularGraph(sensor: sensor);
     }
   }
 }
--- App/uke_mlab/lib/widgets/graph_container/graph_view.dart
+++ App/uke_mlab/lib/widgets/graph_container/graph_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:uke_mlab/models/system_state.dart';
-import 'package:uke_mlab/widgets/graph/graph.dart';
 import 'package:uke_mlab/widgets/graph/graph_adder.dart';
 import 'package:uke_mlab/widgets/graph/graph_adder_popup.dart';
 import 'graph_container.dart';
